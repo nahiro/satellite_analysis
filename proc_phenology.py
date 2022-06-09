da9d23f7466086fed4d7727eec6839827b23c7d9
Naohiro Manago <manago.naohiro@gmail.com>
2022-06-10 03:15:22 +0900

    NM added files.

---
+++ proc_phenology.py
@@ -0,0 +1,38 @@
+from run_phenology import Phenology
+
+proc_phenology = Phenology()
+proc_phenology.proc_name = 'phenology'
+proc_phenology.proc_title = 'Estimate Growth Stage'
+proc_phenology.pnams.append('inp_fnam')
+proc_phenology.pnams.append('atc_params')
+proc_phenology.pnams.append('atc_ithrs')
+proc_phenology.pnams.append('atc_nthrs')
+proc_phenology.params['inp_fnam'] = 'Input File'
+proc_phenology.params['atc_params'] = 'Parameter for ATC'
+proc_phenology.params['atc_ithrs'] = 'Id Threshold for ATC'
+proc_phenology.params['atc_nthrs'] = '\u03B4NDVI Threshold for ATC'
+proc_phenology.param_types['inp_fnam'] = 'string'
+proc_phenology.param_types['atc_params'] = 'float_list'
+proc_phenology.param_types['atc_ithrs'] = 'int_list'
+proc_phenology.param_types['atc_nthrs'] = 'float_list'
+proc_phenology.param_range['atc_params'] = (-1000.0,1000.0)
+proc_phenology.param_range['atc_ithrs'] = (0,1000)
+proc_phenology.param_range['atc_nthrs'] = (-1.0,1.0)
+proc_phenology.defaults['inp_fnam'] = 'input.tif'
+proc_phenology.defaults['atc_params'] = [90.0,10.0]
+proc_phenology.defaults['atc_ithrs'] = [30,35]
+proc_phenology.defaults['atc_nthrs'] = [-0.0005,-0.0005,-0.0003]
+proc_phenology.list_sizes['atc_params'] = 2
+proc_phenology.list_sizes['atc_ithrs'] = 2
+proc_phenology.list_sizes['atc_nthrs'] = 3
+proc_phenology.list_labels['atc_params'] = ['Ratio (%) :',' Offset :']
+proc_phenology.list_labels['atc_ithrs'] = ['T1 :',' T2 :']
+proc_phenology.list_labels['atc_nthrs'] = ['T1 :',' T2 :',' T3 :']
+proc_phenology.input_types['inp_fnam'] = 'ask_file'
+proc_phenology.input_types['atc_params'] = 'float_list'
+proc_phenology.input_types['atc_ithrs'] = 'int_list'
+proc_phenology.input_types['atc_nthrs'] = 'float_list'
+for pnam in proc_phenology.pnams:
+    proc_phenology.values[pnam] = proc_phenology.defaults[pnam]
+proc_phenology.left_frame_width = 210
+proc_phenology.middle_left_frame_width = 1000
