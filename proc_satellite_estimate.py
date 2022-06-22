e8478e5330ce00aa101c2914554ccc845081da14
Naohiro Manago <manago.naohiro@gmail.com>
2022-06-23 05:26:20 +0900

    NM modified files.

--- proc_satellite_estimate.py
+++ proc_satellite_estimate.py
@@ -8,8 +8,8 @@ proc_estimate.pnams.append('data_select')
 proc_estimate.pnams.append('assess_value')
 proc_estimate.pnams.append('mature_value')
 proc_estimate.pnams.append('age_value')
-proc_estimate.pnams.append('intens_fnam')
-proc_estimate.pnams.append('intens_number')
+proc_estimate.pnams.append('pm_fnam')
+proc_estimate.pnams.append('pm_number')
 proc_estimate.pnams.append('digitize')
 proc_estimate.pnams.append('y_params')
 proc_estimate.pnams.append('score_max')
@@ -19,8 +19,8 @@ proc_estimate.params['data_select'] = 'Data Selection Criteria'
 proc_estimate.params['assess_value'] = 'Days from Assessment (day)'
 proc_estimate.params['mature_value'] = 'Days from Heading (day)'
 proc_estimate.params['age_value'] = 'Days from Planting (day)'
-proc_estimate.params['intens_fnam'] = 'Plot-mean/Score-mean Formula'
-proc_estimate.params['intens_number'] = 'P-m/S-m Formula Number'
+proc_estimate.params['pm_fnam'] = 'Plot-mean Formula'
+proc_estimate.params['pm_number'] = 'Plot-mean Formula Number'
 proc_estimate.params['digitize'] = 'Digitize Score'
 proc_estimate.params['y_params'] = 'Output Variable'
 proc_estimate.params['score_max'] = 'Max Output Score'
@@ -30,8 +30,8 @@ proc_estimate.param_types['data_select'] = 'string_select'
 proc_estimate.param_types['assess_value'] = 'float'
 proc_estimate.param_types['mature_value'] = 'float'
 proc_estimate.param_types['age_value'] = 'float'
-proc_estimate.param_types['intens_fnam'] = 'string'
-proc_estimate.param_types['intens_number'] = 'int'
+proc_estimate.param_types['pm_fnam'] = 'string'
+proc_estimate.param_types['pm_number'] = 'int'
 proc_estimate.param_types['digitize'] = 'boolean'
 proc_estimate.param_types['y_params'] = 'boolean_list'
 proc_estimate.param_types['score_max'] = 'int_list'
@@ -39,7 +39,7 @@ proc_estimate.param_types['score_step'] = 'int_list'
 proc_estimate.param_range['assess_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['mature_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['age_value'] = (-1000.0,1000.0)
-proc_estimate.param_range['intens_number'] = (1,10000)
+proc_estimate.param_range['pm_number'] = (1,10000)
 proc_estimate.param_range['score_max'] = (1,65535)
 proc_estimate.param_range['score_step'] = (1,65535)
 proc_estimate.defaults['gis_fnam'] = 'All_area_polygon_20210914.shp'
@@ -47,8 +47,8 @@ proc_estimate.defaults['data_select'] = 'Days from Assessment'
 proc_estimate.defaults['assess_value'] = 0.0
 proc_estimate.defaults['mature_value'] = 35.0
 proc_estimate.defaults['age_value'] = 95.0
-proc_estimate.defaults['intens_fnam'] = 'intensity_formula.csv'
-proc_estimate.defaults['intens_number'] = 1
+proc_estimate.defaults['pm_fnam'] = 'pm_formula_age_90_110.csv'
+proc_estimate.defaults['pm_number'] = 1
 proc_estimate.defaults['digitize'] = False
 proc_estimate.defaults['y_params'] = [True,False,False,False,False,False]
 proc_estimate.defaults['score_max'] = [9,9,1,1,1,9]
@@ -66,8 +66,8 @@ proc_estimate.input_types['data_select'] = 'string_select'
 proc_estimate.input_types['assess_value'] = 'box'
 proc_estimate.input_types['mature_value'] = 'box'
 proc_estimate.input_types['age_value'] = 'box'
-proc_estimate.input_types['intens_fnam'] = 'ask_file'
-proc_estimate.input_types['intens_number'] = 'box'
+proc_estimate.input_types['pm_fnam'] = 'ask_file'
+proc_estimate.input_types['pm_number'] = 'box'
 proc_estimate.input_types['digitize'] = 'boolean'
 proc_estimate.input_types['y_params'] = 'boolean_list'
 proc_estimate.input_types['score_max'] = 'int_list'
