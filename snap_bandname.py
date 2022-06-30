76052f34c548a4eeb502e7a8f3ecaaf7dde65880
Naohiro Manago <manago.naohiro@gmail.com>
2022-06-30 18:49:40 +0900

    NM modified files.

--- snap_bandname.py
+++ snap_bandname.py
@@ -2,24 +2,25 @@
 import os
 import psutil
 mem_size = int(psutil.virtual_memory().available*0.8e-6)
-os.environ['_JAVA_OPTIONS'] = '-Xmx{}m'.format(mem_size)     # Save memory for JAVA
-os.system('export _JAVA_OPTIONS=-Xmx{}m'.format(mem_size))   # Save memory for JAVA
+topdir = os.getcwd()
+options = '-Xmx{}m'.format(mem_size) # Save memory for JAVA
+options += ' -Djava.io.tmpdir='+topdir # Temporary directory
+os.environ['_JAVA_OPTIONS'] = options
+if os.name == 'nt':
+    os.system('set _JAVA_OPTIONS="{}"'.format(options))
+else:
+    os.system('export _JAVA_OPTIONS="{}"'.format(options))
 import sys
 from snappy import ProductIO
-from optparse import OptionParser,IndentedHelpFormatter
+from argparse import ArgumentParser,RawTextHelpFormatter
 
 # Read options
-parser = OptionParser(formatter=IndentedHelpFormatter(max_help_position=200,width=200))
-parser.set_usage('Usage: %prog filename [options]')
-(opts,args) = parser.parse_args()
-if len(args) < 1:
-    parser.print_help()
-    sys.exit(0)
-fnams = args[:]
+parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
+parser.add_argument('inp_fnam',default=None,help='Input file name (%(default)s)')
+args = parser.parse_args()
 
-for fnam in fnams:
-    # Read original product
-    data = ProductIO.readProduct(os.path.abspath(fnam))
-    band_list = list(data.getBandNames())
-    for i,band in enumerate(band_list):
-        sys.stdout.write(os.path.basename(fnam)+' '+'{:4d}'.format(i+1)+' '+band+'\n')
+# Read original product
+data = ProductIO.readProduct(os.path.abspath(args.inp_fnam))
+band_list = list(data.getBandNames())
+for i,band in enumerate(band_list):
+    sys.stdout.write('{:2d} {}\n'.format(i+1,band))
