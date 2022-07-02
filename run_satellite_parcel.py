9caf5ddaab62aff6f812678e9d0e22085fb51aae
Naohiro Manago <manago.naohiro@gmail.com>
2022-07-02 12:57:39 +0900

    NM modified files.

--- run_satellite_parcel.py
+++ run_satellite_parcel.py
@@ -142,7 +142,7 @@ class Parcel(Satellite_Process):
                     raise ValueError('Error, no such file >>> {}'.format(mask_fnam))
                 command = self.python_path
                 command += ' "{}"'.format(os.path.join(self.scr_dir,'sentinel2_parcellate.py'))
-                command += ' --src_geotiff "{}"'.format(os.path.join(dnam,'{}_indices.tif'.format(dstr)))
+                command += ' --src_geotiff "{}"'.format(fnam)
                 command += ' --mask_geotiff "{}"'.format(mask_fnam)
                 command += ' --shp_fnam "{}"'.format(self.values['gis_fnam'])
                 command += ' --out_csv "{}"'.format(gnam)
