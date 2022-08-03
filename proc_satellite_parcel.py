0c883e9faace4623b465810d4e41c1ae5e4671b9
Naohiro Manago <manago.naohiro@gmail.com>
2022-08-03 13:34:27 +0900

    NM modified files.

--- proc_satellite_parcel.py
+++ proc_satellite_parcel.py
@@ -7,12 +7,16 @@ proc_parcel.proc_title = 'Parcellate Data'
 proc_parcel.pnams.append('gis_fnam')
 proc_parcel.pnams.append('band_fnam')
 proc_parcel.pnams.append('out_refs')
+proc_parcel.pnams.append('cr_sc_refs')
+proc_parcel.pnams.append('cr_ref_refs')
 proc_parcel.pnams.append('norm_bands')
 proc_parcel.pnams.append('out_nrefs')
+proc_parcel.pnams.append('cr_sc_nrefs')
+proc_parcel.pnams.append('cr_ref_nrefs')
 proc_parcel.pnams.append('rgi_red_band')
 proc_parcel.pnams.append('out_inds')
-proc_parcel.pnams.append('sc_flag')
-proc_parcel.pnams.append('cloud_flag')
+proc_parcel.pnams.append('cr_sc_inds')
+proc_parcel.pnams.append('cr_ref_inds')
 proc_parcel.pnams.append('cloud_band')
 proc_parcel.pnams.append('cloud_thr')
 proc_parcel.pnams.append('buffer')
@@ -20,12 +24,16 @@ proc_parcel.pnams.append('oflag')
 proc_parcel.params['gis_fnam'] = 'Polygon File'
 proc_parcel.params['band_fnam'] = 'Sentinel-2 Band File'
 proc_parcel.params['out_refs'] = 'Output Reflectance'
+proc_parcel.params['cr_sc_refs'] = 'CR-SC Reflactance'
+proc_parcel.params['cr_ref_refs'] = 'CR-Ref Reflactance'
 proc_parcel.params['norm_bands'] = 'Bands for Normalization'
 proc_parcel.params['out_nrefs'] = 'Output Norm. Reflectance'
+proc_parcel.params['cr_sc_nrefs'] = 'CR-SC Norm. Reflactance'
+proc_parcel.params['cr_ref_nrefs'] = 'CR-Ref Norm. Reflactance'
 proc_parcel.params['rgi_red_band'] = 'Band for RGI'
 proc_parcel.params['out_inds'] = 'Output Index'
-proc_parcel.params['sc_flag'] = 'Cloud Rem. by SC Flag'
-proc_parcel.params['cloud_flag'] = 'Cloud Rem. by Reflectance'
+proc_parcel.params['cr_sc_inds'] = 'CR-SC Index'
+proc_parcel.params['cr_ref_inds'] = 'CR-Ref Index'
 proc_parcel.params['cloud_band'] = 'Band for Cloud Removal'
 proc_parcel.params['cloud_thr'] = 'Thres. for Cloud Removal'
 proc_parcel.params['buffer'] = 'Buffer Radius (m)'
@@ -33,12 +41,16 @@ proc_parcel.params['oflag'] = 'Overwrite Flag'
 proc_parcel.param_types['gis_fnam'] = 'string'
 proc_parcel.param_types['band_fnam'] = 'string'
 proc_parcel.param_types['out_refs'] = 'boolean_list'
+proc_parcel.param_types['cr_sc_refs'] = 'boolean_list'
+proc_parcel.param_types['cr_ref_refs'] = 'boolean_list'
 proc_parcel.param_types['norm_bands'] = 'boolean_list'
 proc_parcel.param_types['out_nrefs'] = 'boolean_list'
+proc_parcel.param_types['cr_sc_nrefs'] = 'boolean_list'
+proc_parcel.param_types['cr_ref_nrefs'] = 'boolean_list'
 proc_parcel.param_types['rgi_red_band'] = 'string'
 proc_parcel.param_types['out_inds'] = 'boolean_list'
-proc_parcel.param_types['sc_flag'] = 'boolean_list'
-proc_parcel.param_types['cloud_flag'] = 'boolean_list'
+proc_parcel.param_types['cr_sc_inds'] = 'boolean_list'
+proc_parcel.param_types['cr_ref_inds'] = 'boolean_list'
 proc_parcel.param_types['cloud_band'] = 'string_select'
 proc_parcel.param_types['cloud_thr'] = 'float'
 proc_parcel.param_types['buffer'] = 'float'
@@ -48,43 +60,59 @@ proc_parcel.param_range['buffer'] = (0.0,10.0e3)
 proc_parcel.defaults['gis_fnam'] = 'All_area_polygon_20210914.shp'
 proc_parcel.defaults['band_fnam'] = 'band_names.txt'
 proc_parcel.defaults['out_refs'] = [True,True,True,True,True,True,True,True,True,True]
+proc_parcel.defaults['cr_sc_refs'] = [True,True,True,True,True,True,True,True,True,True]
+proc_parcel.defaults['cr_ref_refs'] = [True,True,True,True,True,True,True,True,True,True]
 proc_parcel.defaults['norm_bands'] = [True,True,True,True,True,True,True,False,False,False]
 proc_parcel.defaults['out_nrefs'] = [True,True,True,True,True,True,True,True,True,True]
+proc_parcel.defaults['cr_sc_nrefs'] = [True,True,True,True,True,True,True,True,True,True]
+proc_parcel.defaults['cr_ref_nrefs'] = [True,True,True,True,True,True,True,True,True,True]
 proc_parcel.defaults['rgi_red_band'] = 'e1'
 proc_parcel.defaults['out_inds'] = [True,True,True,True]
-proc_parcel.defaults['sc_flag'] = [True,True,True]
-proc_parcel.defaults['cloud_flag'] = [True,True,True]
+proc_parcel.defaults['cr_sc_inds'] = [True,True,True,True]
+proc_parcel.defaults['cr_ref_inds'] = [True,True,True,True]
 proc_parcel.defaults['cloud_band'] = 'r'
 proc_parcel.defaults['cloud_thr'] = 0.35
 proc_parcel.defaults['buffer'] = 1.0
 proc_parcel.defaults['oflag'] = [False,False]
 proc_parcel.list_sizes['out_refs'] = 10
+proc_parcel.list_sizes['cr_sc_refs'] = 10
+proc_parcel.list_sizes['cr_ref_refs'] = 10
 proc_parcel.list_sizes['norm_bands'] = 10
 proc_parcel.list_sizes['out_nrefs'] = 10
+proc_parcel.list_sizes['cr_sc_nrefs'] = 10
+proc_parcel.list_sizes['cr_ref_nrefs'] = 10
 proc_parcel.list_sizes['rgi_red_band'] = 10
 proc_parcel.list_sizes['out_inds'] = 4
-proc_parcel.list_sizes['sc_flag'] = 3
-proc_parcel.list_sizes['cloud_flag'] = 3
+proc_parcel.list_sizes['cr_sc_inds'] = 4
+proc_parcel.list_sizes['cr_ref_inds'] = 4
 proc_parcel.list_sizes['cloud_band'] = 10
 proc_parcel.list_sizes['oflag'] = 2
 proc_parcel.list_labels['out_refs'] = ['b  ','g  ','r  ','e1  ','e2  ','e3  ','n1  ','n2  ','s1  ','s2']
+proc_parcel.list_labels['cr_sc_refs'] = ['b  ','g  ','r  ','e1  ','e2  ','e3  ','n1  ','n2  ','s1  ','s2']
+proc_parcel.list_labels['cr_ref_refs'] = ['b  ','g  ','r  ','e1  ','e2  ','e3  ','n1  ','n2  ','s1  ','s2']
 proc_parcel.list_labels['norm_bands'] = ['b  ','g  ','r  ','e1  ','e2  ','e3  ','n1  ','n2  ','s1  ','s2']
 proc_parcel.list_labels['out_nrefs'] = ['Nb  ','Ng  ','Nr  ','Ne1  ','Ne2  ','Ne3  ','Nn1  ','Nn2  ','Ns1  ','Ns2']
+proc_parcel.list_labels['cr_sc_nrefs'] = ['Nb  ','Ng  ','Nr  ','Ne1  ','Ne2  ','Ne3  ','Nn1  ','Nn2  ','Ns1  ','Ns2']
+proc_parcel.list_labels['cr_ref_nrefs'] = ['Nb  ','Ng  ','Nr  ','Ne1  ','Ne2  ','Ne3  ','Nn1  ','Nn2  ','Ns1  ','Ns2']
 proc_parcel.list_labels['rgi_red_band'] = ['b','g','r','e1','e2','e3','n1','n2','s1','s2']
 proc_parcel.list_labels['out_inds'] = ['NDVI  ','GNDVI  ','RGI  ','NRGI  ']
-proc_parcel.list_labels['sc_flag'] = ['Reflectance','Norm. Reflectance','Index']
-proc_parcel.list_labels['cloud_flag'] = ['Reflectance','Norm. Reflectance','Index']
+proc_parcel.list_labels['cr_sc_inds'] = ['NDVI  ','GNDVI  ','RGI  ','NRGI  ']
+proc_parcel.list_labels['cr_ref_inds'] = ['NDVI  ','GNDVI  ','RGI  ','NRGI  ']
 proc_parcel.list_labels['cloud_band'] = ['b','g','r','e1','e2','e3','n1','n2','s1','s2']
 proc_parcel.list_labels['oflag'] = ['indices','parcel']
 proc_parcel.input_types['gis_fnam'] = 'ask_file'
 proc_parcel.input_types['band_fnam'] = 'ask_file'
 proc_parcel.input_types['out_refs'] = 'boolean_list'
+proc_parcel.input_types['cr_sc_refs'] = 'boolean_list'
+proc_parcel.input_types['cr_ref_refs'] = 'boolean_list'
 proc_parcel.input_types['norm_bands'] = 'boolean_list'
 proc_parcel.input_types['out_nrefs'] = 'boolean_list'
+proc_parcel.input_types['cr_sc_nrefs'] = 'boolean_list'
+proc_parcel.input_types['cr_ref_nrefs'] = 'boolean_list'
 proc_parcel.input_types['rgi_red_band'] = 'string_select'
 proc_parcel.input_types['out_inds'] = 'boolean_list'
-proc_parcel.input_types['sc_flag'] = 'boolean_list'
-proc_parcel.input_types['cloud_flag'] = 'boolean_list'
+proc_parcel.input_types['cr_sc_inds'] = 'boolean_list'
+proc_parcel.input_types['cr_ref_inds'] = 'boolean_list'
 proc_parcel.input_types['cloud_band'] = 'string_select'
 proc_parcel.input_types['cloud_thr'] = 'box'
 proc_parcel.input_types['buffer'] = 'box'
