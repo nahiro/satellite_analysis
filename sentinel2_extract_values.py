64091ca33745dd731eb8e6cc159a6dbeb5be98fa
Naohiro Manago <manago.naohiro@gmail.com>
2022-08-24 17:33:39 +0900

    NM modified files.

--- sentinel2_extract_values.py
+++ sentinel2_extract_values.py
@@ -22,6 +22,7 @@ EPSILON = 1.0e-6 # a small number
 # Default values
 OBS_FNAM = 'observation.csv'
 NCHECK = 10
+RMAX = 50.0 # m
 
 # Read options
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
@@ -32,18 +33,16 @@ parser.add_argument('-o','--tobs',default=None,help='Observation date in the for
 parser.add_argument('-I','--inpdir',default=None,help='Input directory (%(default)s)')
 parser.add_argument('-T','--tendir',default=None,help='Tentative data directory (%(default)s)')
 parser.add_argument('-n','--ncheck',default=NCHECK,type=int,help='Number of plots to check (%(default)s)')
+parser.add_argument('-R','--rmax',default=RMAX,type=float,help='Maximum distance between bunch and plot center in m (%(default)s)')
 parser.add_argument('-O','--out_csv',default=None,help='Output CSV name (%(default)s)')
 parser.add_argument('-F','--fignam',default=None,help='Output figure name for debug (%(default)s)')
-parser.add_argument('-z','--ax1_zmin',default=None,type=float,action='append',help='Axis1 Z min for debug (%(default)s)')
-parser.add_argument('-Z','--ax1_zmax',default=None,type=float,action='append',help='Axis1 Z max for debug (%(default)s)')
-parser.add_argument('-s','--ax1_zstp',default=None,type=float,action='append',help='Axis1 Z stp for debug (%(default)s)')
 parser.add_argument('-t','--ax1_title',default=None,help='Axis1 title for debug (%(default)s)')
 parser.add_argument('--use_index',default=False,action='store_true',help='Use index instead of OBJECTID (%(default)s)')
 parser.add_argument('-H','--header_none',default=False,action='store_true',help='Read csv file with no header (%(default)s)')
 parser.add_argument('-d','--debug',default=False,action='store_true',help='Debug mode (%(default)s)')
 parser.add_argument('-b','--batch',default=False,action='store_true',help='Batch mode (%(default)s)')
 args = parser.parse_args()
-if args.out_csv is None or args.out_shp is None or args.fignam is None:
+if args.out_csv is None or args.fignam is None:
     bnam = 'assess'
     if args.out_csv is None:
         args.out_csv = bnam+'_extract.csv'
@@ -131,21 +130,24 @@ if not np.array_equal(df.iloc[:,0].astype(int),object_ids):
 inp_data = df.iloc[:,1:].astype(float).values # (NOBJECT,NBAND)
 params = columns[1:]
 
-out_nb = len(params)
 out_data = {}
+out_plot = {}
+if args.debug:
+    out_inds = {}
+    out_weight = {}
 for plot in plots:
     cnd = (plot_bunch == plot)
     indx = indx_bunch[cnd]
     ng = number_bunch[indx]
     xg = x_bunch[indx]
     yg = y_bunch[indx]
-    observe_ids = []
-    observe_points = {}
+    observe_inds = []
+    observe_nums = {}
     for n,x,y in zip(ng,xg,yg):
         point = np.array([[x,y]])
         r2 = np.square(x_center-x)+np.square(y_center-y)
         inds = np.argsort(r2)[:args.ncheck]
-        observe_id = None
+        observe_indx = None
         for indx in inds:
             shp = r.shape(indx)
             if len(shp.points) < 1:
@@ -160,61 +162,55 @@ for plot in plots:
                 for p in poly_buffer.geoms:
                     path_search = np.array(p.exterior.coords.xy).swapaxes(0,1)
                     if points_in_poly(point,path_search)[0]:
-                        if observe_id is None:
-                            observe_id = object_ids[indx]
+                        if observe_indx is None:
+                            observe_indx = indx
                         else:
                             sys.stderr.write('Warning, bunch number {} is inside multiple plots.'.format(n))
                             sys.stderr.flush()
             else:
                 path_search = np.array(poly_buffer.exterior.coords.xy).swapaxes(0,1)
                 if points_in_poly(point,path_search)[0]:
-                    if observe_id is None:
-                        observe_id = object_ids[indx]
+                    if observe_indx is None:
+                        observe_indx = indx
                     else:
                         sys.stderr.write('Warning, bunch number {} is inside multiple plots.'.format(n))
                         sys.stderr.flush()
-        #print(n,inds+1,observe_id)
-        if observe_id is None:
+        if observe_indx is None:
             sys.stderr.write('Warning, failed in finding plot for bunch number {}.\n'.format(n))
             sys.stderr.flush()
-            indx = inds[0]
-            observe_id = object_ids[indx]
-        if observe_id in observe_ids:
-            observe_points[observe_id] += 1
+            if np.sqrt(r2[inds[0]]) < args.rmax:
+                observe_indx = inds[0]
+            else:
+                continue
+        if observe_indx in observe_inds:
+            observe_nums[observe_indx] += 1
         else:
-            observe_ids.append(observe_id)
-            observe_points[observe_id] = 1
-    observe_ids = np.array(observe_ids)
-    if len(observe_ids) < 1:
-        pass
-    elif len(observe_ids) == 1:
-        if args.use_index:
-            indx = observe_ids[0]-1
-        else:
-            indx = list_ids.index(observe_id)
-        out_data[plot] = inp_data[indx]
+            observe_inds.append(observe_indx)
+            observe_nums[observe_indx] = 1
+    observe_inds = np.array(observe_inds)
+    if observe_inds.size < 1:
+        raise ValueError('Error in finding correspondent plot for plot{}.'.format(plot))
+    elif observe_inds.size == 1:
+        observe_indx = observe_inds[0]
+        out_data[plot] = inp_data[observe_indx]
+        out_plot[plot] = object_ids[observe_indx]
     else:
-        if args.use_index:
-            indx = observe_ids-1
-        else:
-            indx = []
-            for observe_id in observe_ids:
-                indx.append(object_ids.index(observe_id))
         weight = []
-        for observe_id in observe_ids:
-            weight.append(observe_points[observe_id])
+        for observe_indx in observe_inds:
+            weight.append(observe_nums[observe_indx])
         weight = np.array(weight).reshape(-1,1)
-        cnd = ~np.isnan(inp_data[indx])
+        cnd = ~np.isnan(inp_data[observe_inds])
         if np.all(cnd):
-            out_data[plot] = np.nansum(inp_data[indx]*weight,axis=0)/np.sum(np.int32(cnd)*weight,axis=0)
+            out_data[plot] = np.sum(inp_data[observe_inds]*weight,axis=0)/np.sum(weight)
         else:
-            out_data[plot] = np.nansum(inp_data[indx]*weight,axis=0)/np.sum(np.int32(cnd)*weight,axis=0)
-
-    if plot == 2:
-        break
-
-print('HEREEEEE')
-sys.exit()
+            out_data[plot] = np.nansum(inp_data[observe_inds]*weight,axis=0)/np.sum(np.int32(cnd)*weight,axis=0)
+        out_plot[plot] = object_ids[observe_inds[np.argmax(weight)]]
+    if args.debug:
+        out_inds[plot] = observe_inds.copy()
+        if observe_inds.size == 1:
+            out_weight[plot] = None
+        else:
+            out_weight[plot] = weight.copy()
 
 # Output CSV
 with open(args.out_csv,'w') as fp:
@@ -222,85 +218,70 @@ with open(args.out_csv,'w') as fp:
     for param in params:
         fp.write(', {:>13s}'.format(param))
     fp.write('\n')
-    for iobj,object_id in enumerate(object_ids):
+    for plot in plots:
+        object_id = out_plot[plot]
         fp.write('{:8d}'.format(object_id))
         for iband,param in enumerate(params):
-            fp.write(', {:>13.6e}'.format(out_data[iobj,iband]))
+            fp.write(', {:>13.6e}'.format(out_data[plot][iband]))
         fp.write('\n')
 
-# Output Shapefile
-if args.shp_fnam is not None and args.out_shp is not None:
-    w = shapefile.Writer(args.out_shp)
-    w.shapeType = shapefile.POLYGON
-    w.fields = r.fields[1:] # skip first deletion field
-    for param in params:
-        w.field(param,'F',13,6)
-    for iobj,shaperec in enumerate(r.iterShapeRecords()):
-        rec = shaperec.record
-        shp = shaperec.shape
-        rec.extend(list(out_data[iobj]))
-        w.shape(shp)
-        w.record(*rec)
-    w.close()
-    shutil.copy2(os.path.splitext(args.shp_fnam)[0]+'.prj',os.path.splitext(args.out_shp)[0]+'.prj')
-
 # For debug
-if args.shp_fnam is not None and args.debug:
+if args.debug:
     if not args.batch:
         plt.interactive(True)
     fig = plt.figure(1,facecolor='w',figsize=(5,5))
     plt.subplots_adjust(top=0.9,bottom=0.1,left=0.05,right=0.80)
     pdf = PdfPages(args.fignam)
-    for iband,param in enumerate(params):
-        data = out_data[:,iband]
+    for plot in plots:
+        cnd = (plot_bunch == plot)
+        indx = indx_bunch[cnd]
+        lg = loc_bunch[indx]
+        ng = number_bunch[indx]
+        xg = x_bunch[indx]
+        yg = y_bunch[indx]
         fig.clear()
         ax1 = plt.subplot(111)
         ax1.set_xticks([])
         ax1.set_yticks([])
-        if args.ax1_zmin is not None and not np.isnan(ax1_zmin[param]):
-            zmin = ax1_zmin[param]
+        if out_inds[plot].size == 1:
+            indx = out_inds[plot][0]
+            shp = r.shape(indx)
+            ax1.add_patch(plt.Polygon(shp.points,facecolor='none',edgecolor='k',linewidth=2.0))
+            fig_xmin,fig_ymin,fig_xmax,fig_ymax = shp.bbox
         else:
-            zmin = np.nanmin(data)
-            if np.isnan(zmin):
-                zmin = 0.0
-        if args.ax1_zmax is not None and not np.isnan(ax1_zmax[param]):
-            zmax = ax1_zmax[param]
-        else:
-            zmax = np.nanmax(data)
-            if np.isnan(zmax):
-                zmax = 1.0
-        zdif = zmax-zmin
-        for iobj,shaperec in enumerate(r.iterShapeRecords()):
-            rec = shaperec.record
-            shp = shaperec.shape
-            z = data[iobj]
-            if not np.isnan(z):
-                ax1.add_patch(plt.Polygon(shp.points,edgecolor='none',facecolor=cm.jet((z-zmin)/zdif),linewidth=0.02))
-        im = ax1.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=zmin,vmax=zmax,cmap=cm.jet)
-        divider = make_axes_locatable(ax1)
-        cax = divider.append_axes('right',size='5%',pad=0.05)
-        if args.ax1_zstp is not None and not np.isnan(ax1_zstp[param]):
-            if args.ax1_zmin is not None and not np.isnan(ax1_zmin[param]):
-                zmin = (np.floor(ax1_zmin[param]/ax1_zstp[param])-1.0)*ax1_zstp[param]
-            else:
-                zmin = (np.floor(np.nanmin(data)/ax1_zstp[param])-1.0)*ax1_zstp[param]
-            if args.ax1_zmax is not None and not np.isnan(ax1_zmax[param]):
-                zmax = ax1_zmax[param]+0.1*ax1_zstp[param]
-            else:
-                zmax = np.nanmax(data)+0.1*ax1_zstp[param]
-            ax2 = plt.colorbar(im,cax=cax,ticks=np.arange(zmin,zmax,ax1_zstp[param])).ax
-        else:
-            ax2 = plt.colorbar(im,cax=cax).ax
-        ax2.minorticks_on()
-        ax2.set_ylabel('{}'.format(param))
-        ax2.yaxis.set_label_coords(4.5,0.5)
-        fig_xmin,fig_ymin,fig_xmax,fig_ymax = r.bbox
+            zmin = out_weight[plot].min()
+            zmax = out_weight[plot].max()
+            zdif = zmax-zmin
+            fig_xmin = 1.0e10
+            fig_xmax = -1.0e10
+            fig_ymin = 1.0e10
+            fig_ymax = -1.0e10
+            for iobj,indx in enumerate(out_inds[plot]):
+                shp = r.shape(indx)
+                z = out_weight[plot][iobj]
+                ax1.add_patch(plt.Polygon(shp.points,facecolor='none',edgecolor=cm.jet((z-zmin)/zdif),linewidth=2.0))
+                x1,y1,x2,y2 = shp.bbox
+                if x1 < fig_xmin:
+                    fig_xmin = x1
+                if x2 > fig_xmax:
+                    fig_xmax = x2
+                if y1 < fig_ymin:
+                    fig_ymin = y1
+                if y2 > fig_ymax:
+                    fig_ymax = y2
+        ax1.plot(xg,yg,'o',ms=10,mfc='none',mec='k')
+        for ntmp,xtmp,ytmp in zip(ng,xg,yg):
+            if np.isnan(xtmp) or np.isnan(ytmp):
+                continue
+            ax1.text(xtmp,ytmp,'{}'.format(ntmp))
         ax1.set_xlim(fig_xmin,fig_xmax)
         ax1.set_ylim(fig_ymin,fig_ymax)
         if args.ax1_title is not None:
-            ax1.set_title(args.ax1_title)
+            ax1.set_title('{} (Plot{})'.format(args.ax1_title,plot))
+        else:
+            ax1.set_title('Location: {}, Plot: {}'.format(lg[0],plot))
         plt.savefig(pdf,format='pdf')
         if not args.batch:
             plt.draw()
-            plt.pause(0.1)
+            plt.pause(1.1)
     pdf.close()
