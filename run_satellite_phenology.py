1523f3867124c3296f43a9b6375fa591bc91a2dd
Naohiro Manago <manago.naohiro@gmail.com>
2022-08-26 17:50:42 +0900

    NM modified files.

--- run_satellite_phenology.py
+++ run_satellite_phenology.py
@@ -30,11 +30,12 @@ class Phenology(Satellite_Process):
         if not os.path.exists(mask_fnam):
             raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,mask_fnam))
 
-        # Select reference
+        # Select reference for planting
+        planting_ref = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}_ref.tif'.format(start_dtim,end_dtim))
         command = self.python_path
         command += ' "{}"'.format(os.path.join(self.scr_dir,'trans_select_reference.py'))
         command += ' --datdir "{}"'.format(os.path.join(self.s1_analysis,'planting'))
-        command += ' --dst_fnam "{}"'.format(os.path.join(self.s1_analysis,'planting','ref_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim)))
+        command += ' --dst_fnam "{}"'.format(planting_ref)
         command += ' --mask_fnam "{}"'.format(mask_fnam)
         command += ' --tmin {:%Y%m%d}'.format(start_dtim)
         command += ' --tmax {:%Y%m%d}'.format(end_dtim)
@@ -51,21 +52,23 @@ class Phenology(Satellite_Process):
             command += ' --risetime_max {}'.format(self.values['trans_thr3'][1])
         self.run_command(command,message='<<< Select reference for planting >>>')
 
-        # Calculate average
+        # Calculate average for planting
+        planting_avg = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}_avg.tif'.format(start_dtim,end_dtim))
         command = self.python_path
         command += ' "{}"'.format(os.path.join(self.scr_dir,'trans_average_reference.py'))
-        command += ' --ref_fnam "{}"'.format(os.path.join(self.s1_analysis,'planting','ref_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim)))
-        command += ' --dst_fnam "{}"'.format(os.path.join(self.s1_analysis,'planting','avg_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim)))
+        command += ' --ref_fnam "{}"'.format(planting_ref)
+        command += ' --dst_fnam "{}"'.format(planting_avg)
         command += ' --tmin {:%Y%m%d}'.format(start_dtim)
         command += ' --tmax {:%Y%m%d}'.format(end_dtim)
         self.run_command(command,message='<<< Calculate average for planting >>>')
 
         # Select planting
+        planting_sel = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim))
         command = self.python_path
         command += ' "{}"'.format(os.path.join(self.scr_dir,'trans_select_all.py'))
         command += ' --datdir "{}"'.format(os.path.join(self.s1_analysis,'planting'))
-        command += ' --stat_fnam "{}"'.format(os.path.join(self.s1_analysis,'planting','avg_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim)))
-        command += ' --dst_fnam "{}"'.format(os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}.tif'.format(start_dtim,end_dtim)))
+        command += ' --stat_fnam "{}"'.format(planting_avg)
+        command += ' --dst_fnam "{}"'.format(planting_sel)
         command += ' --mask_fnam "{}"'.format(mask_fnam)
         command += ' --tmin {:%Y%m%d}'.format(start_dtim)
         command += ' --tmax {:%Y%m%d}'.format(end_dtim)
@@ -81,6 +84,23 @@ class Phenology(Satellite_Process):
         if not np.isnan(self.values['trans_thr4'][1]):
             command += ' --risetime_max {}'.format(self.values['trans_thr4'][1])
         self.run_command(command,message='<<< Select planting >>>')
+
+        # Parcellate planting data
+        out_csv = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}.csv'.format(start_dtim,end_dtim))
+        out_shp = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}.shp'.format(start_dtim,end_dtim))
+        out_pdf = os.path.join(self.s1_analysis,'planting','planting_{:%Y%m%d}_{:%Y%m%d}.pdf'.format(start_dtim,end_dtim))
+        command = self.python_path
+        command += ' "{}"'.format(os.path.join(self.scr_dir,'trans_parcellate.py'))
+        command += ' --shp_fnam "{}"'.format(self.values['gis_fnam'])
+        command += ' --src_geotiff "{}"'.format(planting_sel)
+        command += ' --mask_geotiff "{}"'.format(self.values['mask_fnam'])
+        command += ' --out_csv "{}"'.format(out_csv)
+        command += ' --out_shp "{}"'.format(out_shp)
+        command += ' --fignam "{}"'.format(out_pdf)
+        command += ' --use_index'
+        command += ' --remove_nan'
+        command += ' --debug'
+        command += ' --batch'
 
         # Finish process
         sys.stderr.write('Finished process {}.\n\n'.format(self.proc_name))
