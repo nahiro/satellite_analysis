8bf06d0bab1dcd58d369a52b6030f548bedd0436
Naohiro Manago <manago.naohiro@gmail.com>
2022-08-30 01:16:48 +0900

    NM modified files.

--- proc_satellite_estimate.py
+++ proc_satellite_estimate.py
@@ -13,10 +13,7 @@ proc_estimate.pnams.append('plant_value')
 proc_estimate.pnams.append('age_value')
 proc_estimate.pnams.append('pm_fnam')
 proc_estimate.pnams.append('pm_number')
-proc_estimate.pnams.append('digitize')
 proc_estimate.pnams.append('y_params')
-proc_estimate.pnams.append('score_max')
-proc_estimate.pnams.append('score_step')
 proc_estimate.params['gis_fnam'] = 'Polygon File'
 proc_estimate.params['data_select'] = 'Data Selection Criteria'
 proc_estimate.params['harvest_value'] = 'Days from Harvesting (day)'
@@ -27,10 +24,7 @@ proc_estimate.params['plant_value'] = 'Days from Planting (day)'
 proc_estimate.params['age_value'] = 'Age Value (day)'
 proc_estimate.params['pm_fnam'] = 'Plot-mean Formula'
 proc_estimate.params['pm_number'] = 'Plot-mean Formula Number'
-proc_estimate.params['digitize'] = 'Digitize Score'
 proc_estimate.params['y_params'] = 'Output Variable'
-proc_estimate.params['score_max'] = 'Max Output Score'
-proc_estimate.params['score_step'] = 'Score Step for Digitization'
 proc_estimate.param_types['gis_fnam'] = 'string'
 proc_estimate.param_types['data_select'] = 'string_select'
 proc_estimate.param_types['harvest_value'] = 'float'
@@ -41,10 +35,7 @@ proc_estimate.param_types['plant_value'] = 'float'
 proc_estimate.param_types['age_value'] = 'float'
 proc_estimate.param_types['pm_fnam'] = 'string'
 proc_estimate.param_types['pm_number'] = 'int'
-proc_estimate.param_types['digitize'] = 'boolean'
 proc_estimate.param_types['y_params'] = 'boolean_list'
-proc_estimate.param_types['score_max'] = 'int_list'
-proc_estimate.param_types['score_step'] = 'int_list'
 proc_estimate.param_range['harvest_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['assess_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['head_value'] = (-1000.0,1000.0)
@@ -52,8 +43,6 @@ proc_estimate.param_range['peak_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['plant_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['age_value'] = (-1000.0,1000.0)
 proc_estimate.param_range['pm_number'] = (1,10000)
-proc_estimate.param_range['score_max'] = (1,65535)
-proc_estimate.param_range['score_step'] = (1,65535)
 proc_estimate.defaults['gis_fnam'] = 'All_area_polygon_20210914.shp'
 proc_estimate.defaults['data_select'] = 'Days from Assessment'
 proc_estimate.defaults['harvest_value'] = -10.0
@@ -64,18 +53,11 @@ proc_estimate.defaults['plant_value'] = 95.0
 proc_estimate.defaults['age_value'] = 95.0
 proc_estimate.defaults['pm_fnam'] = 'pm_formula_age_90_110.csv'
 proc_estimate.defaults['pm_number'] = 1
-proc_estimate.defaults['digitize'] = False
 proc_estimate.defaults['y_params'] = [True,False,False,False,False,False]
-proc_estimate.defaults['score_max'] = [9,9,1,1,1,9]
-proc_estimate.defaults['score_step'] = [2,2,1,1,1,2]
 proc_estimate.list_sizes['data_select'] = 3
 proc_estimate.list_sizes['y_params'] = 6
-proc_estimate.list_sizes['score_max'] = 6
-proc_estimate.list_sizes['score_step'] = 6
 proc_estimate.list_labels['data_select'] = ['Days from Assessment','Days from Heading','Days from Planting']
 proc_estimate.list_labels['y_params'] = ['BLB','Blast','Borer','Rat','Hopper','Drought']
-proc_estimate.list_labels['score_max'] = ['BLB :',' Blast :',' Borer :',' Rat :',' Hopper :',' Drought :']
-proc_estimate.list_labels['score_step'] = ['BLB :',' Blast :',' Borer :',' Rat :',' Hopper :',' Drought :']
 proc_estimate.input_types['gis_fnam'] = 'ask_file'
 proc_estimate.input_types['data_select'] = 'string_select'
 proc_estimate.input_types['harvest_value'] = 'box'
@@ -86,10 +68,7 @@ proc_estimate.input_types['plant_value'] = 'box'
 proc_estimate.input_types['age_value'] = 'box'
 proc_estimate.input_types['pm_fnam'] = 'ask_file'
 proc_estimate.input_types['pm_number'] = 'box'
-proc_estimate.input_types['digitize'] = 'boolean'
 proc_estimate.input_types['y_params'] = 'boolean_list'
-proc_estimate.input_types['score_max'] = 'int_list'
-proc_estimate.input_types['score_step'] = 'int_list'
 for pnam in proc_estimate.pnams:
     proc_estimate.values[pnam] = proc_estimate.defaults[pnam]
 proc_estimate.left_frame_width = 210
--- run_satellite_estimate.py
+++ run_satellite_estimate.py
@@ -1,16 +1,14 @@
 import os
 import sys
-try:
-    import gdal
-except Exception:
-    from osgeo import gdal
-from subprocess import call
 from proc_satellite_class import Satellite_Process
 
 class Estimate(Satellite_Process):
 
     def __init__(self):
         super().__init__()
+        self.ax1_zmin = None
+        self.ax1_zmax = None
+        self.ax1_zstp = None
         self._freeze()
 
     def run(self):
@@ -20,175 +18,54 @@ class Estimate(Satellite_Process):
         # Check files
         if not os.path.exists(self.values['inp_fnam']):
             raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,self.values['inp_fnam']))
-        if not os.path.exists(self.values['score_fnam']):
-            raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,self.values['score_fnam']))
-        if not os.path.exists(self.values['intensity_fnam']):
-            raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,self.values['intensity_fnam']))
-        trg_bnam = '{}_{}'.format(self.obs_block,self.obs_date)
-        etc_dir = os.path.join(self.drone_analysis,self.obs_block,self.obs_date,self.proc_name)
-        wrk_dir = os.path.join(self.drone_analysis,self.proc_name)
-        if not os.path.exists(etc_dir):
-            os.makedirs(etc_dir)
-        if not os.path.isdir(etc_dir):
-            raise ValueError('{}: error, no such folder >>> {}'.format(self.proc_name,etc_dir))
+        if not os.path.exists(self.values['pm_fnam']):
+            raise IOError('{}: error, no such file >>> {}'.format(self.proc_name,self.values['pm_fnam']))
+        trg_bnam = '{:%Y%m%d}_{:%Y%m%d}'.format(start_dtim,end_dtim)
+        wrk_dir = os.path.join(self.s2_analysis,self.proc_name)
         if not os.path.exists(wrk_dir):
             os.makedirs(wrk_dir)
         if not os.path.isdir(wrk_dir):
             raise ValueError('{}: error, no such folder >>> {}'.format(self.proc_name,wrk_dir))
 
-        # Make mask
-        mask_fnam = os.path.join(etc_dir,'{}_mask.tif'.format(trg_bnam))
+        # Select data
+        select_fnam = os.path.join(wrk_dir,'{}_select.csv'.format(trg_bnam))
         command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'make_mask.py'))
-        command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
-        command += ' --src_geotiff {}'.format(self.values['inp_fnam'])
-        command += ' --dst_geotiff {}'.format(mask_fnam)
-        command += ' --buffer="{}"'.format(-abs(self.values['buffer']))
-        command += ' --use_index'
-        sys.stderr.write('\nMake mask\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
-
-        # Make rebinned image
-        img_fnam = os.path.join(etc_dir,'{}_resized.tif'.format(trg_bnam))
-        ds = gdal.Open(self.values['inp_fnam'])
-        inp_trans = ds.GetGeoTransform()
-        inp_shape = (ds.RasterYSize,ds.RasterXSize)
-        ds = None
-        inp_xmin = inp_trans[0]
-        inp_xstp = inp_trans[1]
-        inp_xmax = inp_xmin+inp_xstp*inp_shape[1]
-        inp_ymax = inp_trans[3]
-        inp_ystp = inp_trans[5]
-        inp_ymin = inp_ymax+inp_ystp*inp_shape[0]
-        istp = int(abs(self.values['region_size']/inp_xstp)+0.5)
-        jstp = int(abs(self.values['region_size']/inp_ystp)+0.5)
-        if istp != jstp:
-            raise ValueError('{}: error, istp={}, jstp={}'.format(self.proc_name,istp,jstp))
-        command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'rebin_gtiff.py'))
+        command += ' "{}"'.format(os.path.join(self.scr_dir,'rebin_mask.py'))
         command += ' --istp {}'.format(istp)
         command += ' --jstp {}'.format(jstp)
-        command += ' --src_geotiff {}'.format(self.values['inp_fnam'])
-        command += ' --dst_geotiff {}'.format(img_fnam)
-        command += ' --mask_geotiff {}'.format(mask_fnam)
+        command += ' --src_geotiff "{}"'.format(mask_fnam)
+        command += ' --dst_geotiff "{}"'.format(mask_resized_fnam)
         command += ' --rmax 0.1'
-        sys.stderr.write('\nRebin image\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
+        self.run_command(command,message='<<< Make parcel image >>>')
 
-        # Make parcel image
-        mask_resized_fnam = os.path.join(etc_dir,'{}_mask_resized.tif'.format(trg_bnam))
+        # Estimate plot-mean
         command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'rebin_mask.py'))
-        command += ' --istp {}'.format(istp)
-        command += ' --jstp {}'.format(jstp)
-        command += ' --src_geotiff {}'.format(mask_fnam)
-        command += ' --dst_geotiff {}'.format(mask_resized_fnam)
-        command += ' --rmax 0.1'
-        sys.stderr.write('\nMake parcel image\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
-
-        # Estimate score
-        command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'drone_score_estimate.py'))
-        command += ' --inp_fnam {}'.format(self.values['score_fnam'])
-        command += ' --src_geotiff {}'.format(img_fnam)
-        command += ' --dst_geotiff {}'.format(os.path.join(etc_dir,'{}_score.tif'.format(trg_bnam)))
+        command += ' "{}"'.format(os.path.join(self.scr_dir,'drone_score_estimate.py'))
+        command += ' --inp_fnam "{}"'.format(self.values['pm_fnam'])
+        command += ' --src_geotiff "{}"'.format(img_fnam)
+        command += ' --dst_geotiff "{}"'.format(os.path.join(wrk_dir,'{}_pm_mesh.tif'.format(trg_bnam)))
         for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
             if flag:
                 command += ' --y_param {}'.format(param)
-                command += ' --y_number {}'.format(self.values['score_number'])
-        for param,flag,value in zip(self.list_labels['y_params'],self.values['y_params'],self.values['score_max']):
-            if flag:
-                command += ' --smax {}'.format(value)
-        for param,flag,value in zip(self.list_labels['y_params'],self.values['y_params'],self.values['score_step']):
-            if flag:
-                command += ' --sint {}'.format(value)
-        if self.values['digitize']:
-            command += ' --digitize'
-        command += ' --fignam {}'.format(os.path.join(etc_dir,'{}_score.pdf'.format(trg_bnam)))
-        command += ' --remove_nan'
-        command += ' --debug'
-        command += ' --batch'
-        sys.stderr.write('\nEstimate score\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
-
-        # Estimate intensity
-        command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'drone_score_estimate.py'))
-        command += ' --inp_fnam {}'.format(self.values['intensity_fnam'])
-        command += ' --src_geotiff {}'.format(img_fnam)
-        command += ' --dst_geotiff {}'.format(os.path.join(etc_dir,'{}_intensity.tif'.format(trg_bnam)))
-        for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
-            if flag:
-                command += ' --y_param {}'.format(param)
-                command += ' --y_number {}'.format(self.values['intensity_number'])
+                command += ' --y_number {}'.format(self.values['pm_number'])
                 command += ' --smax 1'
-        command += ' --fignam {}'.format(os.path.join(etc_dir,'{}_intensity.pdf'.format(trg_bnam)))
+        command += ' --fignam "{}"'.format(os.path.join(wrk_dir,'{}_pm_mesh.pdf'.format(trg_bnam)))
+        for value,flag in zip(self.ax1_zmin[1],self.values['y_params']):
+            if flag:
+                command += ' --ax1_zmin="{}"'.format(value)
+        for value,flag in zip(self.ax1_zmax[1],self.values['y_params']):
+            if flag:
+                command += ' --ax1_zmax="{}"'.format(value)
+        for value,flag in zip(self.ax1_zstp[1],self.values['y_params']):
+            if flag:
+                command += ' --ax1_zstp="{}"'.format(value)
+        command += ' --ax1_title "{}"'.format(trg_bnam)
         command += ' --remove_nan'
         command += ' --debug'
         command += ' --batch'
-        sys.stderr.write('\nEstimate intensity\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
-
-        # Calculate damage intensity of plot from score
-        command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'drone_damage_calculate.py'))
-        command += ' --src_geotiff {}'.format(os.path.join(etc_dir,'{}_score.tif'.format(trg_bnam)))
-        command += ' --mask_geotiff {}'.format(mask_resized_fnam)
-        command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
-        command += ' --out_csv {}'.format(os.path.join(wrk_dir,'{}_damage_score.csv'.format(trg_bnam)))
-        command += ' --out_shp {}'.format(os.path.join(etc_dir,'{}_damage_score.shp'.format(trg_bnam)))
-        for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
-            if flag:
-                command += ' --y_param {}'.format(param)
-        for param,flag,value in zip(self.list_labels['y_params'],self.values['y_params'],self.values['score_max']):
-            if flag:
-                command += ' --smax {}'.format(value)
-        command += ' --rmax 0.01'
-        command += ' --fignam {}'.format(os.path.join(wrk_dir,'{}_damage_score.pdf'.format(trg_bnam)))
-        command += ' --use_index'
-        command += ' --remove_nan'
-        command += ' --debug'
-        command += ' --batch'
-        sys.stderr.write('\nEstimate intensity of plot from score\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
-
-        # Calculate damage intensity of plot from intensity
-        command = self.python_path
-        command += ' {}'.format(os.path.join(self.scr_dir,'drone_damage_calculate.py'))
-        command += ' --src_geotiff {}'.format(os.path.join(etc_dir,'{}_intensity.tif'.format(trg_bnam)))
-        command += ' --mask_geotiff {}'.format(mask_resized_fnam)
-        command += ' --shp_fnam {}'.format(self.values['gis_fnam'])
-        command += ' --out_csv {}'.format(os.path.join(wrk_dir,'{}_damage_intensity.csv'.format(trg_bnam)))
-        command += ' --out_shp {}'.format(os.path.join(etc_dir,'{}_damage_intensity.shp'.format(trg_bnam)))
-        for param,flag in zip(self.list_labels['y_params'],self.values['y_params']):
-            if flag:
-                command += ' --y_param {}'.format(param)
-                command += ' --smax 1'
-        command += ' --rmax 0.01'
-        command += ' --fignam {}'.format(os.path.join(wrk_dir,'{}_damage_intensity.pdf'.format(trg_bnam)))
-        command += ' --use_index'
-        command += ' --remove_nan'
-        command += ' --debug'
-        command += ' --batch'
-        sys.stderr.write('\nEstimate intensity of plot from intensity\n')
-        sys.stderr.write(command+'\n')
-        sys.stderr.flush()
-        call(command,shell=True)
+        self.run_command(command,message='<<< Estimate plot-mean >>>')
 
         # Finish process
-        sys.stderr.write('Finished process {}.\n\n'.format(self.proc_name))
+        sys.stderr.write('\nFinished process {}.\n\n'.format(self.proc_name))
         sys.stderr.flush()
         return
