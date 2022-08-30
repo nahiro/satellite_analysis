af7501317ffb402e9a87ee547012eca6e72de2f5
Naohiro MANAGO <manago.naohiro@gmail.com>
2022-08-30 17:22:16 +0900

    NM modified files.

--- atcor_select_reference.py
+++ atcor_select_reference.py
@@ -66,3 +66,97 @@ np.save('band3.npy',b3_array)
 np.save('band4.npy',b4_array)
 np.save('band8.npy',b8_array)
 np.save('scl.npy',scl_array)
+
+
+ntim = np.load('ntim.npy')
+b2 = np.load('band2.npy')
+b3 = np.load('band3.npy')
+b4 = np.load('band4.npy')
+b8 = np.load('band8.npy')
+ndvi = (b8.astype(np.float64)-b4.astype(np.float64))/(b8.astype(np.float64)+b4.astype(np.float64))
+nv = ndvi.copy()
+scl = np.load('scl.npy')
+#cnd = (scl < 3.9) | (scl > 7.1)
+cnd = (scl < 1.9) | ((scl > 2.1) & (scl < 3.9)) | (scl > 7.1)
+b2[cnd] = np.nan
+b3[cnd] = np.nan
+b4[cnd] = np.nan
+b8[cnd] = np.nan
+nv[cnd] = np.nan
+b2_mean = np.nanmean(b2.astype(np.float64),axis=0)
+b3_mean = np.nanmean(b3.astype(np.float64),axis=0)
+b4_mean = np.nanmean(b4.astype(np.float64),axis=0)
+b8_mean = np.nanmean(b8.astype(np.float64),axis=0)
+nv_mean = np.nanmean(nv,axis=0)
+b2_std = np.nanstd(b2.astype(np.float64),axis=0)
+b3_std = np.nanstd(b3.astype(np.float64),axis=0)
+b4_std = np.nanstd(b4.astype(np.float64),axis=0)
+b8_std = np.nanstd(b8.astype(np.float64),axis=0)
+nv_std = np.nanstd(nv,axis=0)
+
+np.save('ndvi.npy',ndvi.astype(np.float32))
+np.save('b2.npy',b2)
+np.save('b3.npy',b3)
+np.save('b4.npy',b4)
+np.save('b8.npy',b8)
+np.save('nv.npy',nv.astype(np.float32))
+np.save('b2_mean.npy',b2_mean.astype(np.float32))
+np.save('b3_mean.npy',b3_mean.astype(np.float32))
+np.save('b4_mean.npy',b4_mean.astype(np.float32))
+np.save('b8_mean.npy',b8_mean.astype(np.float32))
+np.save('nv_mean.npy',nv_mean.astype(np.float32))
+np.save('b2_std.npy',b2_std.astype(np.float32))
+np.save('b3_std.npy',b3_std.astype(np.float32))
+np.save('b4_std.npy',b4_std.astype(np.float32))
+np.save('b8_std.npy',b8_std.astype(np.float32))
+np.save('nv_std.npy',nv_std.astype(np.float32))
+
+
+b2_std = np.load('../b2_std.npy')
+b3_std = np.load('../b3_std.npy')
+b4_std = np.load('../b4_std.npy')
+b8_std = np.load('../b8_std.npy')
+
+c2 = (b2_std < 0.035)# | (b2_std < 0.20*b2_mean)
+c3 = (b3_std < 0.035)# | (b3_std < 0.20*b3_mean)
+c4 = (b4_std < 0.035)# | (b4_std < 0.20*b4_mean)
+c8 = (b8_std < 0.035)# | (b8_std < 0.20*b8_mean)
+cnd = c2 & c3 & c4 & c8
+inds = np.indices([cnd.size]).reshape(cnd.shape)
+np.save('inds_selected.npy',inds[cnd])
+
+
+n_nearest = 1000
+
+sid,xc,yc,ndat,leng,area = np.loadtxt('get_center.dat',unpack=True)
+sid = (sid+0.1).astype(np.int64)
+ndat = (ndat+0.1).astype(np.int64)
+sid_indx = np.arange(sid.size)
+
+inds = np.load('inds_selected.npy')
+
+ds = gdal.Open('/home/naohiro/Work/Sentinel-2/L2A/Cihea/resample/20210916_geocor_resample.tif')
+data = ds.ReadAsArray()
+data_trans = ds.GetGeoTransform()
+data_shape = data[0].shape
+ds = None
+indy,indx = np.indices(data_shape)
+xp = data_trans[0]+(indx+0.5)*data_trans[1]+(indy+0.5)*data_trans[2]
+yp = data_trans[3]+(indx+0.5)*data_trans[4]+(indy+0.5)*data_trans[5]
+
+xq = xp.flatten()[inds]
+yq = yp.flatten()[inds]
+
+inds_array = []
+leng_array = []
+for n in range(sid.size):
+    l2 = np.square(xq-xc[n])+np.square(yq-yc[n])
+    indx = np.argsort(l2)
+    inds_temp = inds[indx[:n_nearest]]
+    leng_temp = np.sqrt(l2[indx[:n_nearest]])
+    inds_array.append(inds_temp)
+    leng_array.append(leng_temp)
+inds_array = np.array(inds_array)
+leng_array = np.array(leng_array)
+np.save('nearest_inds.npy',inds_array)
+np.save('nearest_leng.npy',leng_array)
