17c2eed6147739c256ccf53de2a77084abc00110
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-05 21:48:00 +0900

    NM modified files.

--- sentinel2_atcor_fit.py
+++ sentinel2_atcor_fit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 import os
-import sys
 import re
 import zlib # import zlib before gdal to prevent segmentation fault when saving pdf
 try:
     import gdal
 except Exception:
     from osgeo import gdal
-from datetime import datetime
 import warnings
 import numpy as np
-from matplotlib.dates import date2num,num2date
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.offsetbox import AnchoredText
@@ -22,13 +19,10 @@ PARAMS = ['Sb','Sg','Sr','Se1','Se2','Se3','Sn1','Sn2','Ss1','Ss2',
           'Nb','Ng','Nr','Ne1','Ne2','Ne3','Nn1','Nn2','Ns1','Ns2',
           'NDVI','GNDVI','RGI','NRGI']
 S2_PARAM = ['b','g','r','e1','e2','e3','n1','n2','s1','s2']
-S2_BAND = {'b':'B2','g':'B3','r':'B4','e1':'B5','e2':'B6','e3':'B7','n1':'B8','n2':'B8A','s1':'B11','s2':'B12'}
 BAND_NAME = {'b':'Blue','g':'Green','r':'Red','e1':'RedEdge1','e2':'RedEdge2','e3':'RedEdge3','n1':'NIR1','n2':'NIR2','s1':'SWIR1','s2':'SWIR2'}
 
 # Default values
 PARAM = ['Nb','Ng','Nr','Ne1','Ne2','Ne3','Nn1','NDVI','GNDVI','NRGI']
-NORM_BAND = ['b','g','r','e1','e2','e3','n1']
-RGI_RED_BAND = 'e1'
 CR_BAND = 'r'
 VTHR = [
 'Sb:0.02','Sg:0.02','Sr:0.02','Se1:0.02','Se2:0.02','Se3:0.02','Sn1:0.02','Sn2:0.02','Ss1:0.02','Ss2:0.02',
@@ -44,8 +38,6 @@ NFIG = 1000
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
 parser.add_argument('-I','--src_geotiff',default=None,help='Source GeoTIFF name (%(default)s)')
 parser.add_argument('-p','--param',default=None,action='append',help='Output parameter ({})'.format(PARAM))
-parser.add_argument('-N','--norm_band',default=None,action='append',help='Wavelength band for normalization ({})'.format(NORM_BAND))
-parser.add_argument('-r','--rgi_red_band',default=RGI_RED_BAND,help='Wavelength band for RGI (%(default)s)')
 parser.add_argument('-C','--cr_band',default=CR_BAND,help='Wavelength band for cloud removal (%(default)s)')
 parser.add_argument('-c','--cthr',default=CTHR,type=float,help='Threshold for cloud removal (%(default)s)')
 parser.add_argument('-v','--vthr',default=None,type=float,help='Absolute threshold to remove outliers ({})'.format(VTHR))
@@ -59,7 +51,7 @@ parser.add_argument('-t','--ax1_title',default=None,help='Axis1 title for debug 
 parser.add_argument('--mask_fnam',default=None,help='Mask file name (%(default)s)')
 parser.add_argument('--stat_fnam',default=None,help='Statistic file name (%(default)s)')
 parser.add_argument('--inds_fnam',default=INDS_FNAM,help='Index file name (%(default)s)')
-parser.add_argument('-F','--fig_fnam',default=None,help='Output figure name for debug (%(default)s)')
+parser.add_argument('-F','--fignam',default=None,help='Output figure name for debug (%(default)s)')
 parser.add_argument('--nfig',default=NFIG,type=int,help='Max number of figure for debug (%(default)s)')
 parser.add_argument('-o','--out_fnam',default=None,help='Output NPZ name (%(default)s)')
 parser.add_argument('--outlier_remove2',default=False,action='store_true',help='2-step outlier removal mode (%(default)s)')
@@ -71,46 +63,8 @@ if args.param is None:
 for param in args.param:
     if not param in PARAMS:
         raise ValueError('Error, unknown parameter >>> {}'.format(param))
-if args.norm_band is None:
-    args.norm_band = NORM_BAND
-for band in args.norm_band:
-    if not band in S2_BAND:
-        raise ValueError('Error, unknown band for normalization >>> {}'.format(band))
-if not args.rgi_red_band in S2_BAND:
-    raise ValueError('Error, unknown band for rgi >>> {}'.format(args.rgi_red_band))
-if not args.cr_band in S2_BAND:
+if not args.cr_band in S2_PARAM:
     raise ValueError('Error, unknown band for clean-day select >>> {}'.format(args.cr_band))
-inp_band = []
-for param in args.param:
-    if param == 'NDVI':
-        for band in ['r','n1']:
-            if not band in inp_band:
-                inp_band.append(band)
-    elif param == 'GNDVI':
-        for band in ['g','n1']:
-            if not band in inp_band:
-                inp_band.append(band)
-    elif param in ['RGI','NRGI']:
-        for band in ['g',args.rgi_red_band]:
-            if not band in inp_band:
-                inp_band.append(band)
-    elif param[0] in ['S','N']:
-        if len(param) in [2,3]:
-            band = param[1:]
-            if not band in inp_band:
-                inp_band.append(band)
-        else:
-            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
-    else:
-        raise ValueError('Error, param={}'.format(param))
-for band in args.norm_band:
-    if not band in inp_band:
-        inp_band.append(band)
-if not args.cr_band in inp_band:
-    inp_band.append(args.cr_band)
-inp_band = np.array(inp_band)
-indx = np.argsort([S2_PARAM.index(band) for band in inp_band])
-inp_band = inp_band[indx]
 if args.vthr is None:
     args.vthr = VTHR
 vthr = {}
@@ -141,38 +95,27 @@ else:
     while len(args.ax1_ymax) < len(args.param):
         args.ax1_ymax.append(args.ax1_ymax[-1])
 bnam,enam = os.path.splitext(os.path.basename(args.src_geotiff))
-if args.out_fnam is None or args.fig_fnam is None:
+if args.out_fnam is None or args.fignam is None:
     if args.out_fnam is None:
         args.out_fnam = '{}_atcor_fit.npz'.format(bnam)
-    if args.fig_fnam is None:
-        args.fig_fnam = '{}_atcor_fit.pdf'.format(bnam)
+    if args.fignam is None:
+        args.fignam = '{}_atcor_fit.pdf'.format(bnam)
 
 # Read Source GeoTIFF
 ds = gdal.Open(args.src_geotiff)
 src_nx = ds.RasterXSize
 src_ny = ds.RasterYSize
-src_nb = len(inp_band)
+src_nb = ds.RasterCount
 src_shape = (src_ny,src_nx)
 src_prj = ds.GetProjection()
 src_trans = ds.GetGeoTransform()
 if src_trans[2] != 0.0 or src_trans[4] != 0.0:
     raise ValueError('Error, src_trans={} >>> {}'.format(src_trans,args.src_geotiff))
-src_band = [S2_BAND[band] for band in inp_band]
-src_indx = {}
-for band in inp_band:
-    src_indx[band] = src_band.index(S2_BAND[band])
-tmp_nb = ds.RasterCount
-tmp_band = []
-for iband in range(tmp_nb):
+src_band = []
+for iband in range(src_nb):
     band = ds.GetRasterBand(iband+1)
-    tmp_band.append(band.GetDescription())
-src_data = []
-for band in inp_band:
-    iband = tmp_band.index(S2_BAND[band])
-    band = ds.GetRasterBand(iband+1)
-    src_data.append(band.ReadAsArray())
-src_data = np.array(src_data)*1.0e-4 # NBAND,NY,NX
-cr_data = src_data[src_indx[args.cr_band]].copy() # NY,NX
+    src_band.append(band.GetDescription())
+src_data = ds.ReadAsArray() # NBAND,NY,NX
 src_dtype = band.DataType
 src_nodata = band.GetNoDataValue()
 src_meta = ds.GetMetadata()
@@ -185,8 +128,34 @@ src_ymin = src_ymax+src_ny*src_ystp
 ds = None
 if src_nodata is not None and not np.isnan(src_nodata):
     src_data[src_data == src_nodata] = np.nan
+param = 'S'+args.cr_band
+if not param in src_band:
+    raise ValueError('Error in finding {} >>> {}'.format(param,args.src_geotiff))
+cr_data = src_data[src_band.index(param)].copy() # NY,NX
+all_data = []
+for param in args.param:
+    iband = src_band.index(param)
+    all_data.append(src_data[iband])
+all_data = np.array(all_data)
+
 cnd = (np.isnan(cr_data)) | (cr_data > args.cthr)
-src_data[:,cnd] = np.nan
+all_data[:,cnd] = np.nan
+
+# Read Mask GeoTIFF
+if args.mask_fnam is not None:
+    ds = gdal.Open(args.mask_fnam)
+    mask_nx = ds.RasterXSize
+    mask_ny = ds.RasterYSize
+    mask_nb = ds.RasterCount
+    if mask_nb != 1:
+        raise ValueError('Error, mask_nb={} >>> {}'.format(mask_nb,args.mask_fnam))
+    mask_shape = (mask_ny,mask_nx)
+    if mask_shape != src_shape:
+        raise ValueError('Error, mask_shape={}, src_shape={} >>> {}'.format(mask_shape,src_shape,args.mask_fnam))
+    mask_data = ds.ReadAsArray().reshape(mask_ny,mask_nx)
+    ds = None
+    cnd = (mask_data < 0.5)
+    all_data[:,cnd] = np.nan
 
 # Read Statistic GeoTIFF
 ds = gdal.Open(args.stat_fnam)
@@ -202,24 +171,9 @@ for iband in range(stat_nb):
     stat_band.append(band.GetDescription())
 stat_data = ds.ReadAsArray().reshape(stat_nb,stat_ny,stat_nx)
 ds = None
-for band in args.param:
-    if not band in stat_band:
-        raise ValueError('Error in finding {} >>> {}'.format(band,args.stat_fnam))
-
-# Read Mask GeoTIFF
-if args.mask_fnam is not None:
-    ds = gdal.Open(args.mask_fnam)
-    mask_nx = ds.RasterXSize
-    mask_ny = ds.RasterYSize
-    mask_nb = ds.RasterCount
-    if mask_nb != 1:
-        raise ValueError('Error, mask_nb={} >>> {}'.format(mask_nb,args.mask_fnam))
-    mask_shape = (mask_ny,mask_nx)
-    if mask_shape != src_shape:
-        raise ValueError('Error, mask_shape={}, src_shape={} >>> {}'.format(mask_shape,src_shape,args.mask_fnam))
-    mask_data = ds.ReadAsArray().reshape(mask_ny,mask_nx)
-    ds = None
-    src_data[mask_data < 0.5] = np.nan
+for param in args.param:
+    if not param in stat_band:
+        raise ValueError('Error in finding {} >>> {}'.format(param,args.stat_fnam))
 
 # Read Index
 data = np.load(args.inds_fnam)
@@ -227,61 +181,12 @@ nearest_inds = data['inds']
 object_ids = data['object_ids']
 nobject = len(object_ids)
 
-# Calculate indices
-all_nx = src_nx
-all_ny = src_ny
-all_nb = len(args.param)
-all_band = args.param
-all_data = np.full((all_nb,all_ny,all_nx),np.nan)
-norm = 0.0
-for band in args.norm_band:
-    norm += src_data[src_indx[band]]
-norm = len(args.norm_band)/norm
-pnams = []
-for iband,param in enumerate(args.param):
-    if param == 'NDVI':
-        red = src_data[src_indx['r']]
-        nir = src_data[src_indx['n1']]
-        pnams.append(param)
-        all_data[iband] = ((nir-red)/(nir+red))
-    elif param == 'GNDVI':
-        green = src_data[src_indx['g']]
-        nir = src_data[src_indx['n1']]
-        pnams.append(param)
-        all_data[iband] = ((nir-green)/(nir+green))
-    elif param == 'RGI':
-        green = src_data[src_indx['g']]
-        red = src_data[src_indx[args.rgi_red_band]]
-        pnams.append(param)
-        all_data[iband] = (green*red)
-    elif param == 'NRGI':
-        green = src_data[src_indx['g']]
-        red = src_data[src_indx[args.rgi_red_band]]
-        pnams.append(param)
-        all_data[iband] = (green*norm*red*norm)
-    elif param[0] == 'S':
-        if len(param) in [2,3]:
-            band = param[1:]
-            pnams.append('{}'.format(BAND_NAME[band]))
-            all_data[iband] = src_data[src_indx[band]]
-        else:
-            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
-    elif param[0] == 'N':
-        if len(param) in [2,3]:
-            band = param[1:]
-            pnams.append('Normalized {}'.format(BAND_NAME[band]))
-            all_data[iband] = src_data[src_indx[band]]*norm
-        else:
-            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
-    else:
-        raise ValueError('Error, param={}'.format(param))
-
 if args.debug:
     if not args.batch:
         plt.interactive(True)
     fig = plt.figure(1,facecolor='w',figsize=(6,6))
     plt.subplots_adjust(top=0.85,bottom=0.20,left=0.15,right=0.90)
-    pdf = PdfPages(args.fig_fnam)
+    pdf = PdfPages(args.fignam)
     xfit = np.arange(-1.0,float(len(args.norm_band))+0.001,0.01)
     fig_interval = int(np.ceil(nobject/args.nfig)+0.1)
 else:
--- sentinel2_calc_indices.py
+++ sentinel2_calc_indices.py
@@ -16,51 +16,23 @@ from argparse import ArgumentParser,RawTextHelpFormatter
 PARAMS = ['Sb','Sg','Sr','Se1','Se2','Se3','Sn1','Sn2','Ss1','Ss2',
           'Nb','Ng','Nr','Ne1','Ne2','Ne3','Nn1','Nn2','Ns1','Ns2',
           'NDVI','GNDVI','RGI','NRGI']
-band_list = ['b','g','r','e1','e2','e3','n1','n2','s1','s2']
-bands = {}
-bands['b'] = 'Blue'
-bands['g'] = 'Green'
-bands['r'] = 'Red'
-bands['e1'] = 'RedEdge1'
-bands['e2'] = 'RedEdge2'
-bands['e3'] = 'RedEdge3'
-bands['n1'] = 'NIR1'
-bands['n2'] = 'NIR2'
-bands['s1'] = 'SWIR1'
-bands['s2'] = 'SWIR2'
-band_dict = {}
-band_dict['b'] = 'B2'
-band_dict['g'] = 'B3'
-band_dict['r'] = 'B4'
-band_dict['e1'] = 'B5'
-band_dict['e2'] = 'B6'
-band_dict['e3'] = 'B7'
-band_dict['n1'] = 'B8'
-band_dict['n2'] = 'B8A'
-band_dict['s1'] = 'B11'
-band_dict['s2'] = 'B12'
-band_index = {}
-for iband,band in enumerate(band_list):
-    band_index[band] = iband
+S2_PARAM = ['b','g','r','e1','e2','e3','n1','n2','s1','s2']
+S2_BAND = {'b':'B2','g':'B3','r':'B4','e1':'B5','e2':'B6','e3':'B7','n1':'B8','n2':'B8A','s1':'B11','s2':'B12'}
+BAND_NAME = {'b':'Blue','g':'Green','r':'Red','e1':'RedEdge1','e2':'RedEdge2','e3':'RedEdge3','n1':'NIR1','n2':'NIR2','s1':'SWIR1','s2':'SWIR2'}
 
 # Default values
 PARAM = ['Nb','Ng','Nr','Ne1','Ne2','Ne3','Nn1','Nn2','Ns1','Ns2','NDVI','GNDVI','RGI','NRGI']
 NORM_BAND = ['b','g','r','e1','e2','e3','n1']
-BAND_COL = 1
 RGI_RED_BAND = 'e1'
 
 # Read options
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
 parser.add_argument('-I','--src_geotiff',default=None,help='Source GeoTIFF name (%(default)s)')
 parser.add_argument('-O','--dst_geotiff',default=None,help='Destination GeoTIFF name (%(default)s)')
-parser.add_argument('-M','--mask_geotiff',default=None,help='Mask GeoTIFF name (%(default)s)')
-parser.add_argument('-B','--band_fnam',default=None,help='Band file name (%(default)s)')
-parser.add_argument('--band_col',default=BAND_COL,help='Band column number (%(default)s)')
+parser.add_argument('-M','--mask_fnam',default=None,help='Mask GeoTIFF name (%(default)s)')
 parser.add_argument('-p','--param',default=None,action='append',help='Output parameter ({})'.format(PARAM))
 parser.add_argument('-N','--norm_band',default=None,action='append',help='Wavelength band for normalization ({})'.format(NORM_BAND))
 parser.add_argument('-r','--rgi_red_band',default=RGI_RED_BAND,help='Wavelength band for RGI (%(default)s)')
-parser.add_argument('--data_min',default=None,type=float,help='Minimum data value (%(default)s)')
-parser.add_argument('--data_max',default=None,type=float,help='Maximum data value (%(default)s)')
 parser.add_argument('-F','--fignam',default=None,help='Output figure name for debug (%(default)s)')
 parser.add_argument('-z','--ax1_zmin',default=None,type=float,action='append',help='Axis1 Z min for debug (%(default)s)')
 parser.add_argument('-Z','--ax1_zmax',default=None,type=float,action='append',help='Axis1 Z max for debug (%(default)s)')
@@ -79,10 +51,43 @@ for param in args.param:
 if args.norm_band is None:
     args.norm_band = NORM_BAND
 for band in args.norm_band:
-    if not band in bands:
+    if not band in S2_BAND:
         raise ValueError('Error, unknown band for normalization >>> {}'.format(band))
-if not args.rgi_red_band in bands:
-    raise ValueError('Error, unknown band for rgi >>> {}'.format(args.rgi_red_band))
+if not args.rgi_red_band in S2_BAND:
+    raise ValueError('Error, unknown band for RGI >>> {}'.format(args.rgi_red_band))
+if not args.cr_band in S2_BAND:
+    raise ValueError('Error, unknown band for clean-day select >>> {}'.format(args.cr_band))
+inp_band = []
+for param in args.param:
+    if param == 'NDVI':
+        for band in ['r','n1']:
+            if not band in inp_band:
+                inp_band.append(band)
+    elif param == 'GNDVI':
+        for band in ['g','n1']:
+            if not band in inp_band:
+                inp_band.append(band)
+    elif param in ['RGI','NRGI']:
+        for band in ['g',args.rgi_red_band]:
+            if not band in inp_band:
+                inp_band.append(band)
+    elif param[0] in ['S','N']:
+        if len(param) in [2,3]:
+            band = param[1:]
+            if not band in inp_band:
+                inp_band.append(band)
+        else:
+            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
+    else:
+        raise ValueError('Error, param={}'.format(param))
+for band in args.norm_band:
+    if not band in inp_band:
+        inp_band.append(band)
+if not args.cr_band in inp_band:
+    inp_band.append(args.cr_band)
+inp_band = np.array(inp_band)
+indx = np.argsort([S2_PARAM.index(band) for band in inp_band])
+inp_band = inp_band[indx]
 if args.ax1_zmin is not None:
     while len(args.ax1_zmin) < len(args.param):
         args.ax1_zmin.append(args.ax1_zmin[-1])
@@ -101,47 +106,35 @@ elif args.dst_geotiff is None or args.fignam is None:
     if args.fignam is None:
         args.fignam = bnam+'_indices.pdf'
 
-def calc_vpix(data,band):
-    vpix = data[band_index[band]].copy()
-    if args.data_min is not None:
-        vpix[vpix < args.data_min] = np.nan
-    if args.data_max is not None:
-        vpix[vpix > args.data_max] = np.nan
-    return vpix
-
-# Get band name
-band_name = []
-with open(args.band_fnam,'r') as fp:
-    for line in fp:
-        item = line.split()
-        if len(item) <= args.band_col or item[0][0]=='#':
-            continue
-        band_name.append(item[args.band_col])
-
 # Read Source GeoTIFF
 ds = gdal.Open(args.src_geotiff)
 src_nx = ds.RasterXSize
 src_ny = ds.RasterYSize
-src_nb = len(band_list)
+src_nb = len(inp_band)
 src_shape = (src_ny,src_nx)
 src_prj = ds.GetProjection()
 src_trans = ds.GetGeoTransform()
 if src_trans[2] != 0.0 or src_trans[4] != 0.0:
     raise ValueError('Error, src_trans={} >>> {}'.format(src_trans,args.src_geotiff))
-src_meta = ds.GetMetadata()
+src_band = [S2_BAND[band] for band in inp_band]
+src_indx = {}
+for band in inp_band:
+    src_indx[band] = src_band.index(S2_BAND[band])
+tmp_nb = ds.RasterCount
+tmp_band = []
+for iband in range(tmp_nb):
+    band = ds.GetRasterBand(iband+1)
+    tmp_band.append(band.GetDescription())
 src_data = []
-src_band = []
-for band in band_list:
-    bnam = band_dict[band]
-    if not bnam in band_name:
-        raise ValueError('Error, {} is not in the band name list.'.format(bnam))
-    iband = band_name.index(bnam)
-    b = ds.GetRasterBand(iband+1)
-    src_data.append(b.ReadAsArray().astype(np.float64).reshape(src_ny,src_nx))
-    src_band.append(b.GetDescription())
-src_data = np.array(src_data)
-src_dtype = b.DataType
-src_nodata = b.GetNoDataValue()
+for band in inp_band:
+    iband = tmp_band.index(S2_BAND[band])
+    band = ds.GetRasterBand(iband+1)
+    src_data.append(band.ReadAsArray())
+src_data = np.array(src_data)*1.0e-4 # NBAND,NY,NX
+cr_data = src_data[src_indx[args.cr_band]].copy() # NY,NX
+src_dtype = band.DataType
+src_nodata = band.GetNoDataValue()
+src_meta = ds.GetMetadata()
 src_xmin = src_trans[0]
 src_xstp = src_trans[1]
 src_xmax = src_xmin+src_nx*src_xstp
@@ -151,81 +144,81 @@ src_ymin = src_ymax+src_ny*src_ystp
 ds = None
 if src_nodata is not None and not np.isnan(src_nodata):
     src_data[src_data == src_nodata] = np.nan
+cnd = (np.isnan(cr_data)) | (cr_data > args.cthr)
+src_data[:,cnd] = np.nan
 
 # Read Mask GeoTIFF
-if args.mask_geotiff is not None:
-    ds = gdal.Open(args.mask_geotiff)
+if args.mask_fnam is not None:
+    ds = gdal.Open(args.mask_fnam)
     mask_nx = ds.RasterXSize
     mask_ny = ds.RasterYSize
     mask_nb = ds.RasterCount
     if mask_nb != 1:
-        raise ValueError('Error, mask_nb={} >>> {}'.format(mask_nb,args.mask_geotiff))
+        raise ValueError('Error, mask_nb={} >>> {}'.format(mask_nb,args.mask_fnam))
     mask_shape = (mask_ny,mask_nx)
     if mask_shape != src_shape:
-        raise ValueError('Error, mask_shape={}, src_shape={} >>> {}'.format(mask_shape,src_shape,args.mask_geotiff))
+        raise ValueError('Error, mask_shape={}, src_shape={} >>> {}'.format(mask_shape,src_shape,args.mask_fnam))
     mask_data = ds.ReadAsArray().reshape(mask_ny,mask_nx)
     ds = None
     src_data[:,mask_data < 0.5] = np.nan
 
-fnam = args.src_geotiff
-norm = 0.0
-value_pix = {}
-for band in bands:
-    value_pix[band] = calc_vpix(src_data,band)
-    if band in args.norm_band:
-        norm += value_pix[band]
-norm = len(args.norm_band)/norm
-dst_data = []
-pnams = []
-for param in args.param:
-    if param == 'NDVI':
-        red = value_pix['r']
-        nir = value_pix['n1']
-        pnams.append(param)
-        dst_data.append((nir-red)/(nir+red))
-    elif param == 'GNDVI':
-        green = value_pix['g']
-        nir = value_pix['n1']
-        pnams.append(param)
-        dst_data.append((nir-green)/(nir+green))
-    elif param == 'RGI':
-        green = value_pix['g']
-        red = value_pix[args.rgi_red_band]
-        pnams.append(param)
-        dst_data.append(green*red*1.0e-8)
-    elif param == 'NRGI':
-        green = value_pix['g']
-        red = value_pix[args.rgi_red_band]
-        pnams.append(param)
-        dst_data.append(green*norm*red*norm)
-    elif param[0] == 'S':
-        if len(param) in [2,3]:
-            band = param[1:]
-            pnams.append('{}'.format(bands[band]))
-            dst_data.append(value_pix[band]*1.0e-4)
-        else:
-            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
-    elif param[0] == 'N':
-        if len(param) in [2,3]:
-            band = param[1:]
-            pnams.append('Normalized {}'.format(bands[band]))
-            dst_data.append(value_pix[band]*norm)
-        else:
-            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
-    else:
-        raise ValueError('Error, param={}'.format(param))
-dst_data = np.array(dst_data)
-
-# Write Destination GeoTIFF
+# Calculate indices
 dst_nx = src_nx
 dst_ny = src_ny
 dst_nb = len(args.param)
 dst_prj = src_prj
 dst_trans = src_trans
 dst_meta = src_meta
+dst_meta['norm_band'] = ','.join(args.norm_band)
+dst_meta['rgi_red_band'] = args.rgi_red_band
 dst_dtype = gdal.GDT_Float32
 dst_nodata = np.nan
 dst_band = args.param
+dst_data = np.full((dst_nb,dst_ny,dst_nx),np.nan)
+norm = 0.0
+for band in args.norm_band:
+    norm += src_data[src_indx[band]]
+norm = len(args.norm_band)/norm
+pnams = []
+for iband,param in enumerate(args.param):
+    if param == 'NDVI':
+        red = src_data[src_indx['r']]
+        nir = src_data[src_indx['n1']]
+        pnams.append(param)
+        dst_data[iband] = ((nir-red)/(nir+red))
+    elif param == 'GNDVI':
+        green = src_data[src_indx['g']]
+        nir = src_data[src_indx['n1']]
+        pnams.append(param)
+        dst_data[iband] = ((nir-green)/(nir+green))
+    elif param == 'RGI':
+        green = src_data[src_indx['g']]
+        red = src_data[src_indx[args.rgi_red_band]]
+        pnams.append(param)
+        dst_data[iband] = (green*red)
+    elif param == 'NRGI':
+        green = src_data[src_indx['g']]
+        red = src_data[src_indx[args.rgi_red_band]]
+        pnams.append(param)
+        dst_data[iband] = (green*norm*red*norm)
+    elif param[0] == 'S':
+        if len(param) in [2,3]:
+            band = param[1:]
+            pnams.append('{}'.format(BAND_NAME[band]))
+            dst_data[iband] = src_data[src_indx[band]]
+        else:
+            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
+    elif param[0] == 'N':
+        if len(param) in [2,3]:
+            band = param[1:]
+            pnams.append('Normalized {}'.format(BAND_NAME[band]))
+            dst_data[iband] = src_data[src_indx[band]]*norm
+        else:
+            raise ValueError('Error, len(param)={} >>> {}'.format(len(param),param))
+    else:
+        raise ValueError('Error, param={}'.format(param))
+
+# Write Destination GeoTIFF
 drv = gdal.GetDriverByName('GTiff')
 ds = drv.Create(args.dst_geotiff,dst_nx,dst_ny,dst_nb,dst_dtype)
 ds.SetProjection(dst_prj)
