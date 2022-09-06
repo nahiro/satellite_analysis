d7bd5e9eba7648955fc1e6bb5efb60f0a48c8e66
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-07 01:45:10 +0900

    NM modified files.

--- sentinel2_atcor_fit.py
+++ sentinel2_atcor_fit.py
@@ -25,13 +25,12 @@ NORM_BAND = ['b','g','r','e1','e2','e3','n1']
 # Default values
 PARAM = ['Nb','Ng','Nr','Ne1','Ne2','Ne3','Nn1','NDVI','GNDVI','NRGI']
 CR_BAND = 'r'
-VTHR = [
-'Sb:0.02','Sg:0.02','Sr:0.02','Se1:0.02','Se2:0.02','Se3:0.02','Sn1:0.02','Sn2:0.02','Ss1:0.02','Ss2:0.02',
-'Nb:0.05','Ng:0.05','Nr:0.05','Ne1:0.05','Ne2:0.05','Ne3:0.05','Nn1:0.05','Nn2:0.05','Ns1:0.05','Ns2:0.05',
-'NDVI:0.1','GNDVI:0.1','RGI:0.1','NRGI:0.1']
-RTHR = 1.0
-MTHR = 2.0
 CTHR = 0.35
+VSTP = [
+'Sb:0.01','Sg:0.01','Sr:0.01','Se1:0.01','Se2:0.01','Se3:0.01','Sn1:0.01','Sn2:0.01','Ss1:0.01','Ss2:0.01',
+'Nb:0.01','Ng:0.01','Nr:0.01','Ne1:0.01','Ne2:0.01','Ne3:0.01','Nn1:0.01','Nn2:0.01','Ns1:0.01','Ns2:0.01',
+'NDVI:0.05','GNDVI:0.05','RGI:0.05','NRGI:0.05']
+ETHR = 2.0
 INDS_FNAM = 'nearest_inds.npz'
 NFIG = 1000
 
@@ -41,9 +40,8 @@ parser.add_argument('-I','--src_geotiff',default=None,help='Source GeoTIFF name 
 parser.add_argument('-p','--param',default=None,action='append',help='Output parameter ({})'.format(PARAM))
 parser.add_argument('-C','--cr_band',default=CR_BAND,help='Wavelength band for cloud removal (%(default)s)')
 parser.add_argument('-c','--cthr',default=CTHR,type=float,help='Threshold for cloud removal (%(default)s)')
-parser.add_argument('-v','--vthr',default=None,action='append',help='Absolute threshold to remove outliers ({})'.format(VTHR))
-parser.add_argument('-R','--rthr',default=RTHR,type=float,help='Relative threshold for 2-step outlier removal (%(default)s)')
-parser.add_argument('--mthr',default=MTHR,type=float,help='Multiplying factor of vthr for 2-step outlier removal (%(default)s)')
+parser.add_argument('-v','--vstp',default=None,action='append',help='Level step for fitting ({})'.format(VSTP))
+parser.add_argument('-E','--ethr',default=ETHR,type=float,help='Max error in sigma for fitting (%(default)s)')
 parser.add_argument('-x','--ax1_xmin',default=None,type=float,action='append',help='Axis1 X min for debug (%(default)s)')
 parser.add_argument('-X','--ax1_xmax',default=None,type=float,action='append',help='Axis1 X max for debug (%(default)s)')
 parser.add_argument('-y','--ax1_ymin',default=None,type=float,action='append',help='Axis1 Y min for debug (%(default)s)')
@@ -66,19 +64,19 @@ for param in args.param:
         raise ValueError('Error, unknown parameter >>> {}'.format(param))
 if not args.cr_band in S2_PARAM:
     raise ValueError('Error, unknown band for cloud removal >>> {}'.format(args.cr_band))
-if args.vthr is None:
-    args.vthr = VTHR
-vthr = {}
-for s in args.vthr:
+if args.vstp is None:
+    args.vstp = VSTP
+vstp = {}
+for s in args.vstp:
     m = re.search('\s*(\S+)\s*:\s*(\S+)\s*',s)
     if not m:
         raise ValueError('Error, invalid threshold to remove outliers >>> {}'.format(s))
     param = m.group(1)
     value = float(m.group(2))
     if not param in PARAMS:
-        raise ValueError('Error, unknown objective variable for vthr ({}) >>> {}'.format(param,s))
+        raise ValueError('Error, unknown objective variable for vstp ({}) >>> {}'.format(param,s))
     if not np.isnan(value):
-        vthr[param] = value
+        vstp[param] = value
 if args.ax1_xmin is not None:
     while len(args.ax1_xmin) < len(args.param):
         args.ax1_xmin.append(args.ax1_xmin[-1])
@@ -106,20 +104,35 @@ if args.out_fnam is None or args.fignam is None:
 ds = gdal.Open(args.src_geotiff)
 src_nx = ds.RasterXSize
 src_ny = ds.RasterYSize
-src_nb = ds.RasterCount
+src_nb = len(args.param)
 src_shape = (src_ny,src_nx)
 src_prj = ds.GetProjection()
 src_trans = ds.GetGeoTransform()
 if src_trans[2] != 0.0 or src_trans[4] != 0.0:
     raise ValueError('Error, src_trans={} >>> {}'.format(src_trans,args.src_geotiff))
-src_band = []
-for iband in range(src_nb):
+src_band = args.param
+tmp_nb = ds.RasterCount
+tmp_band = []
+for iband in range(tmp_nb):
     band = ds.GetRasterBand(iband+1)
-    src_band.append(band.GetDescription())
-src_data = ds.ReadAsArray() # NBAND,NY,NX
+    tmp_band.append(band.GetDescription())
+src_data = []
+for param in src_band:
+    if not param in tmp_band:
+        raise ValueError('Error in finding {} in {}'.format(param,args.src_geotiff))
+    iband = tmp_band.index(param)
+    band = ds.GetRasterBand(iband+1)
+    src_data.append(band.ReadAsArray())
+src_data = np.array(src_data) # NBAND,NY,NX
 src_dtype = band.DataType
 src_nodata = band.GetNoDataValue()
 src_meta = ds.GetMetadata()
+param = 'S'+args.cr_band
+if not param in tmp_band:
+    raise ValueError('Error in finding {} in {}'.format(param,args.src_geotiff))
+iband = tmp_band.index(param)
+band = ds.GetRasterBand(iband+1)
+cr_data = band.ReadAsArray() # NY,NX
 src_xmin = src_trans[0]
 src_xstp = src_trans[1]
 src_xmax = src_xmin+src_nx*src_xstp
@@ -129,24 +142,14 @@ src_ymin = src_ymax+src_ny*src_ystp
 ds = None
 if src_nodata is not None and not np.isnan(src_nodata):
     src_data[src_data == src_nodata] = np.nan
-param = 'S'+args.cr_band
-if not param in src_band:
-    raise ValueError('Error in finding {} >>> {}'.format(param,args.src_geotiff))
-cr_data = src_data[src_band.index(param)].copy() # NY,NX
+cnd = (np.isnan(cr_data)) | (cr_data > args.cthr)
+src_data[:,cnd] = np.nan
 if not 'norm_band' in src_meta:
     sys.stderr.write('Warning, failed in finding {} in metadata >>> {}\n'.format('norm_band',args.src_geotiff))
     sys.stderr.flush()
     norm_band = NORM_BAND
 else:
     norm_band = [s.strip() for s in src_meta['norm_band'].split(',')]
-all_data = []
-for param in args.param:
-    iband = src_band.index(param)
-    all_data.append(src_data[iband])
-all_data = np.array(all_data)
-
-cnd = (np.isnan(cr_data)) | (cr_data > args.cthr)
-all_data[:,cnd] = np.nan
 
 # Read Mask GeoTIFF
 if args.mask_fnam is not None:
@@ -162,25 +165,31 @@ if args.mask_fnam is not None:
     mask_data = ds.ReadAsArray().reshape(mask_ny,mask_nx)
     ds = None
     cnd = (mask_data < 0.5)
-    all_data[:,cnd] = np.nan
+    src_data[:,cnd] = np.nan
 
 # Read Statistic GeoTIFF
 ds = gdal.Open(args.stat_fnam)
 stat_nx = ds.RasterXSize
 stat_ny = ds.RasterYSize
-stat_nb = ds.RasterCount
+stat_nb = len(args.param)
 stat_shape = (stat_ny,stat_nx)
 if stat_shape != src_shape:
     raise ValueError('Error, stat_shape={}, src_shape={} >>> {}'.format(stat_shape,src_shape,args.stat_fnam))
-stat_band = []
-for iband in range(stat_nb):
+stat_band = args.param
+tmp_nb = ds.RasterCount
+tmp_band = []
+for iband in range(tmp_nb):
     band = ds.GetRasterBand(iband+1)
-    stat_band.append(band.GetDescription())
-stat_data = ds.ReadAsArray().reshape(stat_nb,stat_ny,stat_nx)
+    tmp_band.append(band.GetDescription())
+stat_data = []
+for param in stat_band:
+    if not param in tmp_band:
+        raise ValueError('Error in finding {} in {}'.format(param,args.src_geotiff))
+    iband = tmp_band.index(param)
+    band = ds.GetRasterBand(iband+1)
+    stat_data.append(band.ReadAsArray())
+stat_data = np.array(stat_data) # NBAND,NY,NX
 ds = None
-for param in args.param:
-    if not param in stat_band:
-        raise ValueError('Error in finding {} >>> {}'.format(param,args.stat_fnam))
 
 # Read Index
 data = np.load(args.inds_fnam)
@@ -225,7 +234,7 @@ factor_array = []
 offset_array = []
 rmse_array = []
 for iband,param in enumerate(args.param):
-    data_x_all = all_data[iband].flatten()
+    data_x_all = src_data[iband].flatten()
     data_y_all = stat_data[iband].flatten()
     data_b_all = cr_data.flatten()
     if data_x_all.shape != data_y_all.shape:
@@ -243,45 +252,69 @@ for iband,param in enumerate(args.param):
         else:
             fig_flag = False
         indx = nearest_inds[iobj]
-        data_x = data_x_all[indx]
-        data_y = data_y_all[indx]
-        data_b = data_b_all[indx]
-        if args.outlier_remove2:
-            cnd1 = (~np.isnan(data_x)) & (np.abs(data_x-data_y) < (np.abs(data_y)*args.rthr).clip(min=vthr[param]*args.mthr))
-        else:
-            cnd1 = ~np.isnan(data_x)
-        xcnd = data_x[cnd1]
-        ycnd = data_y[cnd1]
-        bcnd = data_b[cnd1]
-        if xcnd.size < 2:
+        cnd = (~np.isnan(data_x_all[indx])) & (~np.isnan(data_y_all[indx]))
+        data_x = (data_x_all[indx])[cnd]
+        data_y = (data_y_all[indx])[cnd]
+        data_b = (data_b_all[indx])[cnd]
+        if data_x.size < 3:
+            xs = np.array([])
+            ys = np.array([])
             result = [np.nan,np.nan]
             calc_y = np.array([])
             r_value = np.nan
             cr_value = np.nan
             cr_error = np.nan
             rms_value = np.nan
-            flag = False
         else:
-            result = np.polyfit(xcnd,ycnd,1)
-            calc_y = xcnd*result[0]+result[1]
-            cnd2 = np.abs(calc_y-ycnd) < vthr[param]
-            xcnd2 = xcnd[cnd2]
-            ycnd2 = ycnd[cnd2]
-            bcnd2 = bcnd[cnd2]
-            if (xcnd2.size == cnd2.size) or (xcnd2.size < 2):
-                r_value = np.corrcoef(xcnd,ycnd)[0,1]
-                cr_value = np.nanmean(bcnd)
-                cr_error = np.nanstd(bcnd)
-                rms_value = np.sqrt(np.square(calc_y-ycnd).sum()/calc_y.size)
-                flag = False
+            xs = []
+            ys = []
+            v1 = np.ceil(np.min(data_y)/vstp[param])*vstp[param]
+            v2 = np.floor(np.max(data_y)/vstp[param])*vstp[param]
+            for v in np.arange(v1,v2+0.1*vstp[param],vstp[param]):
+                cnd = np.abs(data_y-v) < 0.5*(vstp[param])
+                xcnd = data_x[cnd]
+                ycnd = data_y[cnd]
+                if xcnd.size > 2:
+                    xm = np.mean(xcnd)
+                    ym = np.mean(ycnd)
+                    xe = np.std(xcnd)
+                    cnd = np.abs(xcnd-xm) < xe*args.ethr
+                    xcnd = xcnd[cnd]
+                    ycnd = ycnd[cnd]
+                    if xcnd.size > 0:
+                        xm = np.mean(xcnd)
+                        ym = np.mean(ycnd)
+                        xe = np.std(xcnd)
+                        cnd = np.abs(xcnd-xm) < xe*args.ethr
+                        xcnd = xcnd[cnd]
+                        ycnd = ycnd[cnd]
+                        if xcnd.size > 0:
+                            xm = np.mean(xcnd)
+                            ym = np.mean(ycnd)
+                            xs.append(xm)
+                            ys.append(ym)
+                        else:
+                            xs.append(xm)
+                            ys.append(ym)
+                    else:
+                        xs.append(xm)
+                        ys.append(ym)
+            xs = np.array(xs)
+            ys = np.array(ys)
+            if xs.size < 2:
+                result = [np.nan,np.nan]
+                calc_y = np.array([])
+                r_value = np.nan
+                cr_value = np.nan
+                cr_error = np.nan
+                rms_value = np.nan
             else:
-                result = np.polyfit(xcnd2,ycnd2,1)
-                calc_y = xcnd2*result[0]+result[1]
-                r_value = np.corrcoef(xcnd2,ycnd2)[0,1]
-                cr_value = np.nanmean(bcnd2)
-                cr_error = np.nanstd(bcnd2)
-                rms_value = np.sqrt(np.square(calc_y-ycnd2).sum()/calc_y.size)
-                flag = True
+                result = np.polyfit(xs,ys,1)
+                calc_y = data_x*result[0]+result[1]
+                r_value = np.corrcoef(data_x,data_y)[0,1]
+                cr_value = np.nanmean(data_b)
+                cr_error = np.nanstd(data_b)
+                rms_value = np.sqrt(np.square(calc_y-data_y).sum()/calc_y.size)
         number.append(calc_y.size)
         corcoef.append(r_value)
         cr_mean.append(cr_value)
@@ -302,34 +335,23 @@ for iband,param in enumerate(args.param):
             at= AnchoredText(line,prop=dict(size=12),loc=2,pad=0.1,borderpad=0.8,frameon=True)
             at.patch.set_boxstyle('round')
             ax1.add_artist(at)
-            if xcnd.size != cnd1.size:
-                ax1.plot(data_x,data_y,'k.')
-            if flag:
-                ax1.plot(xcnd,ycnd,'.',color='#888888')
-                ax1.plot(xcnd2,ycnd2,'b.')
-            else:
-                ax1.plot(xcnd,ycnd,'b.')
+            ax1.plot(data_x,data_y,'b.')
+            ax1.plot(xs,ys,'ro')
             ax1.plot(xfit,np.polyval(result,xfit),'k:')
             if args.ax1_xmin is None or args.ax1_ymin is None:
-                if xcnd.size < 1:
+                if data_x.size < 3:
                     xmin = -1.0
                     ymin = -1.0
-                elif flag:
-                    xmin = min(np.nanmin(xcnd),np.nanmin(xcnd2))
-                    ymin = min(np.nanmin(ycnd),np.nanmin(ycnd2))
                 else:
-                    xmin = np.nanmin(xcnd)
-                    ymin = np.nanmin(ycnd)
+                    xmin = np.min(data_x)
+                    ymin = np.min(data_y)
             if args.ax1_xmax is None or args.ax1_ymax is None:
-                if xcnd.size < 1:
+                if data_x.size < 3:
                     xmax = 1.0
                     ymax = 1.0
-                elif flag:
-                    xmax = max(np.nanmax(xcnd),np.nanmax(xcnd2))
-                    ymax = max(np.nanmax(ycnd),np.nanmax(ycnd2))
                 else:
-                    xmax = np.nanmax(xcnd)
-                    ymax = np.nanmax(ycnd)
+                    xmax = np.max(data_x)
+                    ymax = np.max(data_y)
             if args.ax1_xmin is not None and args.ax1_xmax is not None:
                 ax1.set_xlim(args.ax1_xmin[iband],args.ax1_xmax[iband])
             elif args.ax1_xmin is not None:
