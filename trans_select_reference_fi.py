f07aad36ceba3185c81e74b47837a7bf1a6854b5
Naohiro MANAGO <manago.naohiro@gmail.com>
2022-12-06 10:14:09 +0900

    NM modified files.

--- trans_select_reference_fi.py
+++ trans_select_reference_fi.py
@@ -8,7 +8,11 @@ import xmltodict
 from glob import glob
 from datetime import datetime
 import numpy as np
-from matplotlib.dates import date2num,num2date
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.dates import date2num,num2date,YearLocator,MonthLocator,DayLocator,DateFormatter
+from matplotlib.backends.backend_pdf import PdfPages
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from argparse import ArgumentParser,RawTextHelpFormatter
 
 # Constants
@@ -16,6 +20,9 @@ HOME = os.environ.get('HOME')
 if HOME is None:
     HOME = os.environ.get('USERPROFILE')
 PARAMS = ['trans_d#','bsc_min#','fp_offs#','post_s#','fpi_#']
+PARAM_NAMES = {'trans_d':'Estimated transplanting date (MM/DD)','bsc_min':'Signal at transplanting (dB)','post_s':'Signal after transplanting (dB)','fpi':'Fishpond Index at transplanting'}
+PARAM_MINS = {'trans_d':np.nan,'bsc_min':-25.0,'post_s':0.0,'fpi':0.0}
+PARAM_MAXS = {'trans_d':np.nan,'bsc_min':-10.0,'post_s':5.0,'fpi':1.0}
 OUT_PARAMS = ['OBJECTID','Shape_Leng','Shape_Area','geometry']
 
 # Default values
@@ -41,6 +48,10 @@ parser.add_argument('--post_s_min',default=POST_S_MIN,type=float,help='Min post_
 parser.add_argument('--det_nmin',default=DET_NMIN,type=int,help='Min number of detections (%(default)s)')
 parser.add_argument('--offset',default=OFFSET,type=float,help='Transplanting date offset in day (%(default)s)')
 parser.add_argument('-N','--ncan',default=NCAN,type=int,help='Candidate number between 1 and 3 (%(default)s)')
+parser.add_argument('-F','--fignam',default=None,help='Output figure name for debug (%(default)s)')
+parser.add_argument('-t','--fig_title',default=None,help='Figure title for debug (%(default)s)')
+parser.add_argument('-d','--debug',default=False,action='store_true',help='Debug mode (%(default)s)')
+parser.add_argument('-b','--batch',default=False,action='store_true',help='Batch mode (%(default)s)')
 args = parser.parse_args()
 if args.out_fnam is None:
     raise ValueError('Error, args.out_fnam={}'.format(args.out_fnam))
@@ -236,9 +247,111 @@ for iobj in range(nobject):
     #    break
 
 out_data = data[OUT_PARAMS].copy()
-for i,param in enumerate(params):
+for i,param in enumerate(dst_band):
     out_data[param] = dst_data[i]
 out_data.to_file(args.out_fnam)
 with open('{}.xml'.format(args.out_fnam),'w') as fp:
     dst_meta.update({'@xml:lang':'en'})
     fp.write(xmltodict.unparse({'metadata':dst_meta},pretty=True))
+
+if args.debug:
+    if not args.batch:
+        plt.interactive(True)
+    fig = plt.figure(1,facecolor='w',figsize=(7.6,6.0))
+    plt.subplots_adjust(top=0.95,bottom=0.06,left=0.030,right=0.96,wspace=0.12,hspace=0.15)
+    pdf = PdfPages(args.fignam)
+    fig_xmin,fig_ymin,fig_xmax,fig_ymax = out_data.total_bounds
+    for ncan in range(2):
+        fig.clear()
+        # trans_d
+        if ncan == 1:
+            param = 'p1_2'
+        else:
+            param = 'trans_d'
+        pnam = 'trans_d'
+        data = out_data[param]
+        ax1 = plt.subplot(221)
+        ax1.set_xticks([])
+        ax1.set_yticks([])
+        out_data.plot(column=param,ax=ax1,vmin=nmin,vmax=nmax,cmap=cm.jet)
+        im = ax1.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=nmin,vmax=nmax,cmap=cm.jet)
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes('bottom',size='10%',pad=0.02)
+        ax12 = plt.colorbar(im,cax=cax,orientation='horizontal').ax
+        ax12.minorticks_on()
+        ax12.xaxis.set_major_locator(MonthLocator(bymonth=[1,4,7,10]))
+        ax12.xaxis.set_minor_locator(MonthLocator())
+        ax12.xaxis.set_major_formatter(DateFormatter('%m/%d'))
+        for l in ax12.xaxis.get_ticklabels():
+            l.set_rotation(30)
+        ax12.set_xlabel('{}'.format(PARAM_NAMES[pnam]))
+        ax12.xaxis.set_label_coords(0.5,-3.2)
+        ax1.set_xlim(fig_xmin,fig_xmax)
+        ax1.set_ylim(fig_ymin,fig_ymax)
+        # bsc_min
+        if ncan == 1:
+            param = 'p2_2'
+        else:
+            param = 'bsc_min'
+        pnam = 'bsc_min'
+        ax2 = plt.subplot(222)
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+        out_data.plot(column=param,ax=ax2,vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        im = ax2.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        divider = make_axes_locatable(ax2)
+        cax = divider.append_axes('bottom',size='10%',pad=0.02)
+        ax22 = plt.colorbar(im,cax=cax,orientation='horizontal').ax
+        ax22.minorticks_on()
+        ax22.xaxis.set_major_locator(plt.MultipleLocator(5.0))
+        ax22.set_xlabel('{}'.format(PARAM_NAMES[pnam]))
+        ax22.xaxis.set_label_coords(0.5,-3.2)
+        ax2.set_xlim(fig_xmin,fig_xmax)
+        ax2.set_ylim(fig_ymin,fig_ymax)
+        # post_s
+        if ncan == 1:
+            param = 'p4_2'
+        else:
+            param = 'post_s'
+        pnam = 'post_s'
+        ax3 = plt.subplot(223)
+        ax3.set_xticks([])
+        ax3.set_yticks([])
+        out_data.plot(column=param,ax=ax3,vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        im = ax3.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        divider = make_axes_locatable(ax3)
+        cax = divider.append_axes('bottom',size='10%',pad=0.02)
+        ax32 = plt.colorbar(im,cax=cax,orientation='horizontal').ax
+        ax32.minorticks_on()
+        ax32.set_xlabel('{}'.format(PARAM_NAMES[pnam]))
+        ax32.xaxis.set_label_coords(0.5,-2.2)
+        ax3.set_xlim(fig_xmin,fig_xmax)
+        ax3.set_ylim(fig_ymin,fig_ymax)
+        # fpi
+        if ncan == 1:
+            param = 'p5_2'
+        else:
+            param = 'fpi'
+        pnam = 'fpi'
+        ax4 = plt.subplot(224)
+        ax4.set_xticks([])
+        ax4.set_yticks([])
+        out_data.plot(column=param,ax=ax4,vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        im = ax4.imshow(np.arange(4).reshape(2,2),extent=(-2,-1,-2,-1),vmin=PARAM_MINS[pnam],vmax=PARAM_MAXS[pnam],cmap=cm.jet)
+        divider = make_axes_locatable(ax4)
+        cax = divider.append_axes('bottom',size='10%',pad=0.02)
+        ax42 = plt.colorbar(im,cax=cax,orientation='horizontal').ax
+        ax42.minorticks_on()
+        ax42.set_xlabel('{}'.format(PARAM_NAMES[pnam]))
+        ax42.xaxis.set_label_coords(0.5,-2.2)
+        ax4.set_xlim(fig_xmin,fig_xmax)
+        ax4.set_ylim(fig_ymin,fig_ymax)
+        if args.fig_title is not None:
+            plt.suptitle('#{}, {}'.format(ncan+1,args.fig_title))
+        else:
+            plt.suptitle('#{}'.format(ncan+1))
+        plt.savefig(pdf,format='pdf')
+        if not args.batch:
+            plt.draw()
+            plt.pause(0.1)
+    pdf.close()
