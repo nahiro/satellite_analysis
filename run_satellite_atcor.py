7bd0c72290a6bda61b505cd0f6f9272abc2893e4
Naohiro MANAGO <manago.naohiro@gmail.com>
2023-02-17 06:07:46 +0900

    NM modified files.

--- run_satellite_atcor.py
+++ run_satellite_atcor.py
@@ -83,8 +83,12 @@ class Atcor(Satellite_Process):
                     os.makedirs(dnam)
                 if not os.path.isdir(dnam):
                     raise IOError('Error, no such folder >>> {}'.format(dnam))
-                d2 = datetime(year,1,1)-timedelta(days=1)
-                d1 = d2-timedelta(days=self.values['stat_period'])
+                if self.values['stat_period'] < 0:
+                    d1 = datetime(year,1,1)
+                    d2 = d1+timedelta(days=np.abs(self.values['stat_period']))
+                else:
+                    d2 = datetime(year,1,1)-timedelta(days=1)
+                    d1 = d2-timedelta(days=self.values['stat_period'])
                 command = self.python_path
                 command += ' "{}"'.format(os.path.join(self.scr_dir,'atcor_select_reference.py'))
                 command += ' --shp_fnam "{}"'.format(self.values['gis_fnam'])
@@ -181,8 +185,12 @@ class Atcor(Satellite_Process):
                 else:
                     flag_studyarea = False
                 # Stats
-                d2 = datetime(year,1,1)-timedelta(days=1)
-                d1 = d2-timedelta(days=self.values['stat_period'])
+                if self.values['stat_period'] < 0:
+                    d1 = datetime(year,1,1)
+                    d2 = d1+timedelta(days=np.abs(self.values['stat_period']))
+                else:
+                    d2 = datetime(year,1,1)-timedelta(days=1)
+                    d1 = d2-timedelta(days=self.values['stat_period'])
                 command = self.python_path
                 command += ' "{}"'.format(os.path.join(self.scr_dir,'atcor_calc_stat.py'))
                 command += ' --inpdir "{}"'.format(self.values['indices_dir'])
